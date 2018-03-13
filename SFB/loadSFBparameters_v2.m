dd8114c4b55d5a622fcbcd271ac9fb233128d289
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2018-03-13 01:33:14 +0000

    biovolume figs

---
+++ SFB/loadSFBparameters_v2.m
@@ -0,0 +1,136 @@
+function[phys]=loadSFBparameters_v2(filename)
+
+%filename = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SFB\Data\sfb_raw_2.csv';
+delimiter = ',';
+startRow = 3;
+
+% Read columns of data as text:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
+
+% Open the text file.
+fileID = fopen(filename,'r','n','UTF-8');
+% Skip the BOM (Byte Order Mark).
+fseek(fileID, 3, 'bof');
+
+% Read columns of data according to the format.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'HeaderLines' ,startRow-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+
+% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric text to numbers.
+% Replace non-numeric text with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = mat2cell(dataArray{col}, ones(length(dataArray{col}), 1));
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[3,4,5,6,7,8,9,10,11,12,13,14,15]
+    % Converts text in the input cell array to numbers. Replaced non-numeric
+    % text with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1)
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData(row), regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if numbers.contains(',')
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'))
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric text to numbers.
+            if ~invalidThousandsSeparator
+                numbers = textscan(char(strrep(numbers, ',', '')), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch
+            raw{row, col} = rawData{row};
+        end
+    end
+end
+
+% Convert the contents of columns with dates to MATLAB datetimes using the
+% specified date format.
+try
+    dates{1} = datetime(dataArray{1}, 'Format', 'MM/dd/yy', 'InputFormat', 'MM/dd/yy');
+catch
+    try
+        % Handle dates surrounded by quotes
+        dataArray{1} = cellfun(@(x) x(2:end-1), dataArray{1}, 'UniformOutput', false);
+        dates{1} = datetime(dataArray{1}, 'Format', 'MM/dd/yy', 'InputFormat', 'MM/dd/yy');
+    catch
+        dates{1} = repmat(datetime([NaN NaN NaN]), size(dataArray{1}));
+    end
+end
+
+dates = dates(:,1);
+
+%% Split data into numeric and string columns.
+rawNumericColumns = raw(:, [3,4,5,6,7,8,9,10,11,12,13,14,15]);
+rawStringColumns = string(raw(:, 2));
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),rawNumericColumns); % Find non-numeric cells
+rawNumericColumns(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+Date = dates{:, 1};
+IFCB = rawStringColumns(:, 1);
+StationNumber = cell2mat(rawNumericColumns(:, 1));
+Distancefrom36 = cell2mat(rawNumericColumns(:, 2));
+CalculatedChlorophyll = cell2mat(rawNumericColumns(:, 3));
+CalculatedOxygen = cell2mat(rawNumericColumns(:, 4));
+OpticalBackscatter = cell2mat(rawNumericColumns(:, 5));
+CalculatedSPM = cell2mat(rawNumericColumns(:, 6));
+Salinity = cell2mat(rawNumericColumns(:, 7));
+Temperature = cell2mat(rawNumericColumns(:, 8));
+Nitrite = cell2mat(rawNumericColumns(:, 9));
+NitrateNitrite = cell2mat(rawNumericColumns(:, 10));
+Ammonium = cell2mat(rawNumericColumns(:, 11));
+Phosphate = cell2mat(rawNumericColumns(:, 12));
+Silicate = cell2mat(rawNumericColumns(:, 13));
+
+% For code requiring serial dates (datenum) instead of datetime, uncomment
+% the following line(s) below to return the imported dates as datenum(s).
+
+Date=datenum(Date);
+
+for i=1:length(Date)
+    phys(i).dn=Date(i);
+    phys(i).filename=IFCB(i);
+    phys(i).st=StationNumber(i);
+    phys(i).d36=Distancefrom36(i);    
+    phys(i).sal=Salinity(i);    
+    phys(i).chl=CalculatedChlorophyll(i);
+    phys(i).temp=Temperature(i);
+    phys(i).obs=OpticalBackscatter(i);
+    phys(i).spm=CalculatedSPM(i);
+    phys(i).ni=Nitrite(i);
+    phys(i).nina=NitrateNitrite(i);
+    phys(i).amm=Ammonium(i);
+    phys(i).phos=Phosphate(i);
+    phys(i).sil=Silicate(i);
+end
+
+
+%% Clear temporary variables
+clearvars filename delimiter startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp R;
+
+save('C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SFB\Data\parameters','phys');
+
+end
--- compile_biovolume_yrs.m
+++ compile_biovolume_yrs.m
@@ -1,5 +1,10 @@
-%% compile biovolume multiple years
-path = 'F:\IFCB113\class\summary\'; %Where you want the summary file to go
+function [phyto,p] = compile_biovolume_yrs(biovolume,cruisetime,parameters)
+
+biovolume= 'F:\IFCB113\class\summary\summary_biovol_allcells';
+cruisetime = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SFB\Data\st_filename_raw.csv';
+parameters= 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SFB\Data\sfb_raw_2.csv';
+
+%% compile biovolume multiple years SFB
 
 biovol_sum = [];
 ml_analyzed = [];
@@ -8,7 +13,7 @@ filelist = [];
 
 for yr = 2017:2018
     disp(yr)
-    temp = load([path 'summary_biovol_allcells' num2str(yr)]);
+    temp = load([biovolume num2str(yr)]);
     biovol_sum = [biovol_sum; temp.biovol_sum];
     ml_analyzed = [ ml_analyzed; temp.ml_analyzed];
     matdate = [ matdate; temp.matdate];
@@ -19,10 +24,10 @@ end
 matdate=datenum(datestr(matdate),'dd-mm-yyyy');
 
 %% find overlap between official IFCB stations --> c 
-path = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SFB\Data\';
-[st,filename] = import_IFCB_stations([path 'st_filename_raw.csv']);
 
-[~,b,c] = intersect(filename,{filelist.newname});
+[st,filename] = import_IFCB_stations(cruisetime);
+
+[~,~,c] = intersect(filename,{filelist.newname});
 matdate=matdate(c);
 ml_analyzed=ml_analyzed(c);
 biovol_sum=biovol_sum(c);
@@ -35,17 +40,29 @@ for i=1:length(filename)
     phyto(i).biovol_sum=biovol_sum(i);
 end
 
-save([path 'biovol_param'],'phyto');
+
+%% add sfb parameters to IFCB dataset
+
+[phys]=loadSFBparameters_v2(parameters);
+
+[~,~,c] = intersect([phyto.filename],[phys.filename]);
+
+for i=1:length(c)
+    phyto(i).chl=phys(c(i)).chl;
+    phyto(i).d36=phys(c(i)).d36;
+    phyto(i).temp=phys(c(i)).temp;
+    phyto(i).sal=phys(c(i)).sal;    
+    phyto(i).obs=phys(c(i)).obs;
+    phyto(i).spm=phys(c(i)).spm;
+    phyto(i).ni=phys(c(i)).ni;
+    phyto(i).nina=phys(c(i)).nina;
+    phyto(i).amm=phys(c(i)).amm;
+    phyto(i).phos=phys(c(i)).phos;
+    phyto(i).sil=phys(c(i)).sil;        
+end
 
 
-%%
-[phys]=loadSFBparameters_v2([path 'sfb_raw_2.csv']);
-
-[~,b,c] = intersect([phyto.matdate],[phys.dn]);
-
-
-%% sort data by date
-
+%% sort data by survey dates
 p(1).a=find([phyto.matdate]==datenum('31-Jul-2017'));
 p(2).a=find([phyto.matdate]==datenum('22-Aug-2017'));
 p(3).a=find([phyto.matdate]==datenum('30-Aug-2017'));
@@ -65,52 +82,21 @@ for i=1:length(p)
     p(i).st=[phyto(p(i).a).st]'; 
     p(i).ml_analyzed=[phyto(p(i).a).ml_analyzed]'; 
     p(i).biovol_sum=[phyto(p(i).a).biovol_sum]'; 
+    p(i).chl=[phyto(p(i).a).chl]';
+    p(i).d36=[phyto(p(i).a).d36]';    
+    p(i).temp=[phyto(p(i).a).temp]';
+    p(i).sal=[phyto(p(i).a).sal]';  
+    p(i).obs=[phyto(p(i).a).obs]';
+    p(i).spm=[phyto(p(i).a).spm]';
+    p(i).ni=[phyto(p(i).a).ni]';
+    p(i).nina=[phyto(p(i).a).nina]';
+    p(i).amm=[phyto(p(i).a).amm]';
+    p(i).phos=[phyto(p(i).a).phos]';
+    p(i).sil=[phyto(p(i).a).sil]';    
 end
 p=rmfield(p,'a');
-save([path 'biovol_param'],'phyto','p');
 
-%% attach SFB parameters to IFCB structure 
-%[sfb,s]=loadSFBparameters([path 'sfb_raw.csv']);
-load('sfb.mat');
+save('F:\IFCB113\class\summary\biovol_param','phyto','p');
 
-% link ifcb timepoints with  phys and chem data, c is the overlap
-[~,b,c] = intersect([phyto.matdate],phys.dn);
-lat = sfb.lat(c);
-long = sfb.long(c);
-chl = sfb.chl(c);
-sal = sfb.sal(c);
-obs = sfb.obs(c);
-spm = sfb.spm(c);
-ext = sfb.ext(c);
-
-filename=filename(b);
-st=st(b);
-matdate=matdate(b);
-ml_analyzed(b);
-biovol_sum(b);
-
-phyto = rmfield(phyto,{'filename','st','matdate','ml_analyzed','biovol_sum'});
-for i=1:length(filename)
-    phyto(i).filename=filename(i);
-    phyto(i).st=st(i);
-    phyto(i).matdate=matdate(i);
-    phyto(i).ml_analyzed=ml_analyzed(i);
-    phyto(i).biovol_sum=biovol_sum(i);
-    phyto(i).lat = lat(i);
-    phyto(i).long = long(i);
-    phyto(i).chl = chl(i);
-    phyto(i).sal = sal(i);
-    phyto(i).obs = obs(i);
-    phyto(i).spm = spm(i);
-    phyto(i).ext = ext(i);    
 end
 
-
-
-figure;
-
-for i=1:length(p)
-    plot(p(i).st, p(i).biovol_sum);
-    hold on
-    
-end
