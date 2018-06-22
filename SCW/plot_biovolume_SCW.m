7d1bd599b01eb7b1db119a8eab88fed70d6ba9d2
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2018-06-22 20:19:18 +0000

    updated biovol plots w classifier June 2018

--- SCW/plot_biovolume_SCW.m
+++ SCW/plot_biovolume_SCW.m
@@ -5,27 +5,11 @@
 %% Step 1: Load in data
 figpath = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\';
 resultpath = 'F:\IFCB104\class\summary\'; %Where you want the summary file to go
-load([resultpath 'summary_biovol_allTB2017'],'class2useTB','classcountTB','classbiovolTB','ml_analyzedTB','mdateTB','filelistTB');
+load([resultpath 'summary_biovol_allTB2018'],'class2useTB','classcountTB','classbiovolTB','ml_analyzedTB','mdateTB','filelistTB');
 
-% convert to cubic microns
-micron_factor = 1/3.4; %microns per pixel
-classbiovolTB = classbiovolTB*micron_factor^3;
-
-%% Alternate Step 1
-% Only use if have not already extracted biovolume from class files for specific year
-figpath = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\';
-resultpath = 'F:\IFCB104\class\summary\'; %Where you want the summary file to go
-classpath_generic = 'F:\IFCB104\class\classxxxx_v1\';
-feapath_generic = 'F:\IFCB104\features\xxxx\'; %Put in your featurepath byyear
-roibasepath_generic = 'F:\IFCB104\data\xxxx\'; %Where you raw data is
-year = 2017;
-
-[class2useTB,classcountTB,classbiovolTB,ml_analyzedTB,mdateTB,filelistTB] = extract_biovolume_class(resultpath,classpath_generic,feapath_generic,roibasepath_generic,year);
-% can modify compile_biovolume_yrs or compile_biovolume_summaries if need to compile multiple years of s
-
-% convert to cubic microns
-micron_factor = 1/3.4; %microns per pixel
-classbiovolTB = classbiovolTB*micron_factor^3;
+% % convert to cubic microns
+% micron_factor = 1/3.4; %microns per pixel
+% classbiovolTB = classbiovolTB*micron_factor^3;
 
 %% Step 2: Determine what fraction of Cell-derived biovolume is 
 % Dinoflagellates vs Diatoms vs Classes of interest
@@ -52,14 +36,14 @@ ind = 1; %Akashiwo
 ind = 9; %Ceratium 
     [~,yCER ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
     [~,yCER_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);
-
+    
 ind = 33; %Dinophysis 
     [~,yDINO ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
     [~,yDINO_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);
     
-ind = 24; %Gymnodinium 
-    [~,yGYM ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
-    [~,yGYM_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);
+ind = 26; %Lingulodinium 
+    [~,yLING ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
+    [~,yLING_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);
 
 ind = 33; %Prorocentrum 
     [~,yPRO ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
@@ -73,6 +57,10 @@ ind = 10; %Chaetoceros
 ind = 8; %Centric
     [~,yC ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
     [~,yC_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);
+    
+ind = 34; %Pseudo-nitzschia 
+    [~,yPN ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
+    [~,yPN_ml ] = timeseries2ydmat(mdateTB, ml_analyzedTB);    
     
 ind = 17; %Det_Cer_Lau
     [~,yDETO ] = timeseries2ydmat(mdateTB, classbiovolTB(:,ind));
@@ -92,6 +80,9 @@ subplot = @(m,n,p) subtightplot (m, n, p, [0.04 0.04], [0.08 0.04], [0.09 0.2]);
 %subplot = @(m,n,p) subtightplot(m,n,p,opt{:}); 
 %where opt = {gap, width_h, width_w} describes the inner and outer spacings.  
 
+year=2018; %USER
+%year=2017;
+
 %Fraction dinos and diatoms
 subplot(3,1,1);
 fx_other=[ymat-[ydino+ydiat]]./ymat; %fraction not dinos or diatoms
@@ -100,13 +91,13 @@ bar(xmat, [ydino./ymat ydiat./ymat fx_other], 0.5, 'stack');
 ax = get(gca);
 cat = ax.Children;
 
-cstr = [ [220 220 220]/255; [110 110 110]/255; [0 0 0]/255];
+cstr = [ [220 220 220]/255; [110 110 110]/255; [0 0 0]/255]; %black/dk grey/lt grey
 for ii = 1:length(cat)
     set(cat(ii), 'FaceColor', cstr(ii,:),'BarWidth',1)
 end
 
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2017'; '01-Jul-2017']))
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
 ylim([0;1])
 set(gca,'xticklabel',{}, 'fontsize', 10, 'fontname', 'arial','tickdir','out')
 ylabel('Fraction of total biovolume', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
@@ -116,31 +107,34 @@ hold on
 
 %Fraction dinos and diatoms
 subplot(3,1,2);
-fx_dino=[ydino-[yAKA+yCER+yDINO+yGYM+yPRO]]./ymat; %fraction other dinos
-fx_diat=[ydiat-[yCHAET+yC+yDETO+yEUC+yGUIN]]./ymat; %fraction other diatoms
+fx_dino=[ydino-[yAKA+yCER+yDINO+yLING+yPRO]]./ymat; %fraction other dinos
+fx_diat=[ydiat-[yCHAET+yC+yDETO+yPN+yEUC+yGUIN]]./ymat; %fraction other diatoms
 fx_other=[ymat-[ydino+ydiat]]./ymat; %fraction not dinos or diatoms
 
-bar(xmat,[yAKA./ymat yCER./ymat yDINO./ymat yGYM./ymat yPRO./ymat...
-    yCHAET./ymat yDETO./ymat yEUC./ymat yGUIN./ymat yC./ymat...
+bar(xmat,[yAKA./ymat yCER./ymat yDINO./ymat yLING./ymat yPRO./ymat...
+    yCHAET./ymat yDETO./ymat yEUC./ymat yGUIN./ymat yPN./ymat yC./ymat...
     fx_dino fx_diat fx_other], 0.5, 'stack');
 ax = get(gca);
 cat = ax.Children;
 
-cstr = [[177,89,40]/255; [255,255,153]/255; [166,206,227]/255; [31,120,180]/255;...
+cstr = [[220 220 220]/255; [110 110 110]/255; [0 0 0]/255;...
+    [255,255,153]/255; [166,206,227]/255; [31,120,180]/255;...
     [178,223,138]/255; [51,160,44]/255; [251,154,153]/255; [227,26,28]/255;...
-    [253,191,111]/255; [255,127,0]/255; [202,178,214]/255; [150 150 150]/255; [106,61,154]/255 ];
+    [253,191,111]/255; [255,127,0]/255; [202,178,214]/255;...
+    [106,61,154]/255;[220 220 220]/255];
+
 
 for ii = 1:length(cat)
     set(cat(ii), 'FaceColor', cstr(ii,:),'BarWidth',1)
 end
 
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2017'; '01-Jul-2017']))
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
 ylim([0;1])
 set(gca,'xticklabel',{}, 'fontsize', 10, 'fontname', 'arial','tickdir','out')
 ylabel('Fraction of total biovolume', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
-h=legend('Akashiwo','Ceratium','Dinophysis','Gymnodinium','Prorocentrum',...
-    'Chaetoceros','Detonula','Eucampia','Guinardia','Centric diatoms',...
+h=legend('Akashiwo','Ceratium','Dinophysis','Lingulodinium','Prorocentrum',...
+    'Chaetoceros','Detonula','Eucampia','Guinardia','Pseudo-nitzschia','Centric diatoms',...
     'other dinos','other diatoms','other cell-derived');
 set(h,'Position',[0.808159725831097 0.399956604207141 0.187499996391125 0.246744784681747]);
 hold on
@@ -150,27 +144,29 @@ subplot(3,1,3);
 plot(xmat, ymat./ymat_ml,'k.-','linewidth', 1);
 hold on
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2017'; '01-Jul-2017']))
-ylim([0;1.6*10^4])
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
+%ylim([0;1.6*10^4])
 set(gca,'xgrid','on','fontsize', 10, 'fontname', 'arial','tickdir','out')
 ylabel('Biovolume (\mum^{-3} mL^{-1})', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
 hold all
 
 % set figure parameters
 set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[figpath 'Figs\FxBiovolume_IFCB_SCW_2017.tif']);
+print(gcf,'-dtiff','-r600',[figpath 'Figs\FxBiovolume_IFCB_SCW_' num2str(year) '.tif']);
 hold off
 
 %% plot dinos vs diatoms
 figure, %set(gcf, 'position', [360 278 500 250])
 cstr = ['rbkgcmy']; %order of colors
 
+year=2018; USER
+
 ph = plot(xdino, ydino./ydino_ml,'.-', xdiat, ydiat./ydiat_ml,'.-','linewidth', 1);
 for ii = 1:length(ph)
     set(ph(ii), 'color', cstr(ii))
 end
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2018'; '01-Jul-2018']))
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
 ylabel('Biovolume ( \mum^{ -3} mL^{ -1})', 'fontsize', 16, 'fontname', 'arial')
 legend('dinoflagellates','diatoms','Location','NorthEast')
 set(gca, 'fontsize', 16, 'fontname', 'arial','tickdir','out')
@@ -181,7 +177,8 @@ figure('Units','inches','Position',[1 1 8 5],'PaperPositionMode','auto');
 subplot = @(m,n,p) subtightplot (m, n, p, [0.05 0.05], [0.08 0.04], [0.09 0.2]);
 %subplot = @(m,n,p) subtightplot(m,n,p,opt{:}); 
 %where opt = {gap, width_h, width_w} describes the inner and outer spacings.
-
+year= 218; USER
+ 
 fx_other=[ymat-[ydino+ydiat]]./ymat; %fraction not dinos or diatoms
 
 %Fraction dinos and diatoms
@@ -199,7 +196,7 @@ for ii = 1:length(cat)
 end
 
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2018'; '01-Jul-2018']))
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
 ylim([0;1])
 set(gca,'xticklabel',{}, 'fontsize', 10, 'fontname', 'arial','tickdir','out')
 ylabel('Fraction of total biovolume', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
@@ -212,7 +209,7 @@ subplot(2,1,2);
 plot(xmat, ymat./ymat_ml,'k.-','linewidth', 1);
 hold on
 datetick('x', 3, 'keeplimits')
-xlim(datenum(['01-Jan-2018'; '01-Jul-2018']))
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
 ylim([0;3.2*10^5])
 set(gca,'xgrid','on','fontsize', 10, 'fontname', 'arial','tickdir','out')
 ylabel('Biovolume (\mum^{-3} mL^{-1})', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
@@ -220,6 +217,5 @@ hold all
 
 % set figure parameters
 set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[figpath 'Figs\Diatom_Dino_IFCB_2018.tif']);
+print(gcf,'-dtiff','-r600',[figpath 'Figs\Diatom_Dino_IFCB_' num2str(year) '.tif']);
 hold off
-
--- tools4dataprocessing.m
+++ tools4dataprocessing.m
@@ -1,59 +1,17 @@
-%% Exploratorium
+%% Pipeline to Build, apply, and evaluate classifier
+%  Alexis D. Fischer, University of California - Santa Cruz, June 2018
 
-start_blob_batch_user_training('F:\IFCB113\Exploratorium\data\2018\',...
-    'F:\IFCB113\Exploratorium\blobs\2018\',true)
-
-start_feature_batch_user_training('F:\IFCB113\Exploratorium\data\2018\',...
-    'F:\IFCB113\Exploratorium\blobs\2018\','F:\IFCB113\Exploratorium\features\2018\',true)
-
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
-    'F:\IFCB113\Exploratorium\features\2018\','F:\IFCB113\Exploratorium\class\class2018_v1\')
-
-%% IFCB113 - Extract blobs and features and apply classifier
-
-start_blob_batch_user_training('F:\IFCB113\data\2016\','F:\IFCB113\blobs\2016\',true)
-start_blob_batch_user_training('F:\IFCB113\data\2017\','F:\IFCB113\blobs\2017\',true)
-start_blob_batch_user_training('F:\IFCB113\data\2018\','F:\IFCB113\blobs\2018\',true)
-
-start_feature_batch_user_training('F:\IFCB113\data\2016\',...
-    'F:\IFCB113\blobs\2016\','F:\IFCB113\features\2016\',true)
-start_feature_batch_user_training('F:\IFCB113\data\2017\',...
-    'F:\IFCB113\blobs\2017\','F:\IFCB113\features\2017\',true)
-start_feature_batch_user_training('F:\IFCB113\data\2018\',...
-    'F:\IFCB113\blobs\2018\','F:\IFCB113\features\2018\',true)
-
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
-    'F:\IFCB113\features\2016\','F:\IFCB113\class\class2016_v1\')
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
-    'F:\IFCB113\features\2017\','F:\IFCB113\class\class2017_v1\')
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
-    'F:\IFCB113\features\2018\','F:\IFCB113\class\class2018_v1\')
-
-% Summarize random forest classification results by class
-countcells_allTBnew_user_training(...
-    'F:\IFCB113\class\classxxxx_v1\','F:\IFCB113\data\', 2017:2018)
-
-% Summarize biovolume from forest classification results by class
-biovolume_summary_manual('F:\IFCB113\manual\','F:\IFCB113\data\',...
-    'F:\IFCB113\features\XXXX\');
-
-% Summarize counts for thresholds 0.1 to 1 for the specified class
-yrrange = 2016:2018;
-classpath_generic = 'F:\IFCB113\class\classxxxx_v1\';
-out_path = 'F:\IFCB113\class\summary\'; 
-in_dir = 'F:\IFCB113\data\';
-countcells_allTB_class('Alexandrium_singlet', yrrange, classpath_generic, out_path, in_dir)
-
-%% IFCB104 - Apply and evaluate classifier
+%% PART 1: Applying and evaluating a classifier
+%% Step 1: Sort data into folders
 %sort_data_into_folders;
 
-% Extract blobs
+%% Step 2: Extract blobs
 %start_blob_batch_user_training('F:\IFCB104\data\2015\','F:\IFCB104\blobs\2015\',true)
 %start_blob_batch_user_training('F:\IFCB104\data\2016\','F:\IFCB104\blobs\2016\',true)
 %start_blob_batch_user_training('F:\IFCB104\data\2017\','F:\IFCB104\blobs\2017\',true)
 start_blob_batch_user_training('F:\IFCB104\data\2018\','F:\IFCB104\blobs\2018\',true)
 
-% Extract features
+%% Step 3: Extract features
 % start_feature_batch_user_training('F:\IFCB104\data\2015\',...
 %     'F:\IFCB104\blobs\2015\','F:\IFCB104\features\2015\',true)
 % start_feature_batch_user_training('F:\IFCB104\data\2016\',...
@@ -63,69 +21,81 @@ start_blob_batch_user_training('F:\IFCB104\data\2018\','F:\IFCB104\blobs\2018\',
 start_feature_batch_user_training('F:\IFCB104\data\2018\',...
    'F:\IFCB104\blobs\2018\','F:\IFCB104\features\2018\',true)
 
-% Apply classifier
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
+%% Step 4: Apply classifier
+start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_22Jun2018',...
     'F:\IFCB104\features\2015\','F:\IFCB104\class\class2015_v1\')
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
+start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_22Jun2018',...
     'F:\IFCB104\features\2016\','F:\IFCB104\class\class2016_v1\')
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
+start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_22Jun2018',...
     'F:\IFCB104\features\2017\','F:\IFCB104\class\class2017_v1\')
-start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_08May2018',...
+start_classify_batch_user_training('F:\IFCB104\manual\summary\UserExample_Trees_22Jun2018',...
     'F:\IFCB104\features\2018\','F:\IFCB104\class\class2018_v1\')
 
-%%
-% Summarize random forest classification results by class
+%% Step 5: Summarize random forest classification results by class
 countcells_allTBnew_user_training(...
     'F:\IFCB104\class\classxxxx_v1\','F:\IFCB104\data\', 2015:2018)
 
-% Summarize biovolume from forest classification results by class
+%% Step 6: Summarize biovolume from Manual files
 biovolume_summary_manual('F:\IFCB104\manual\','F:\IFCB104\data\',...
     'F:\IFCB104\features\XXXX\');
-%%
-% Summarize counts for thresholds 0.1 to 1 for the specified class
+
+%% Step 7: Summarize biovolume from Classification results
+% do individual years
+figpath = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\';
+resultpath = 'F:\IFCB104\class\summary\'; %Where you want the summary file to go
+classpath_generic = 'F:\IFCB104\class\classxxxx_v1\';
+feapath_generic = 'F:\IFCB104\features\xxxx\'; %Put in your featurepath byyear
+roibasepath_generic = 'F:\IFCB104\data\xxxx\'; %Where you raw data is
+year = 2018;
+
+extract_biovolume_class(resultpath,classpath_generic,feapath_generic,roibasepath_generic,year);
+% can modify compile_biovolume_yrs or compile_biovolume_summaries if need to compile multiple years
+
+%% Step 8: Summarize counts for thresholds 0.1 to 1 for the specified class
 yrrange = 2015:2018;
 classpath_generic = 'F:\IFCB104\class\classxxxx_v1\';
 out_path = 'F:\IFCB104\class\summary\'; 
 in_dir = 'F:\IFCB104\data\';
+
+%dinos
 countcells_allTB_class('Akashiwo', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Alexandrium_singlet', yrrange, classpath_generic, out_path, in_dir)
 countcells_allTB_class('Ceratium', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Chaetoceros', yrrange, classpath_generic, out_path, in_dir)
 countcells_allTB_class('Dinophysis', yrrange, classpath_generic, out_path, in_dir)
+countcells_allTB_class('Lingulodinium', yrrange, classpath_generic, out_path, in_dir)
 countcells_allTB_class('Prorocentrum', yrrange, classpath_generic, out_path, in_dir)
+
+%diatoms
+countcells_allTB_class('Chaetoceros', yrrange, classpath_generic, out_path, in_dir)
+countcells_allTB_class('Detonula', yrrange, classpath_generic, out_path, in_dir)
 countcells_allTB_class('Pseudo-nitzschia', yrrange, classpath_generic, out_path, in_dir)
-
-countcells_allTB_class('Ditylum', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Ceratium', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Coscinodiscus', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Protoperidinium', yrrange, classpath_generic, out_path, in_dir)
+countcells_allTB_class('Eucampia', yrrange, classpath_generic, out_path, in_dir)
 countcells_allTB_class('Guinardia', yrrange, classpath_generic, out_path, in_dir)
-countcells_allTB_class('Rhizosolenia', yrrange, classpath_generic, out_path, in_dir)
+countcells_allTB_class('Centric', yrrange, classpath_generic, out_path, in_dir)
 
-%% Compile features for the training set
 
+%% PART 2: Training and Making a classifier
+%% Step 1: Compile features for the training set
 manualpath = 'F:\IFCB104\manual\'; % manual annotation file location
 feapath_base = 'F:\IFCB104\features\'; %feature file location, assumes \yyyy\ organization
-maxn = 8000; %maximum number of images per class to include
+maxn = 10000; %maximum number of images per class to include
 minn = 30; %minimum number for inclusion
 class2skip = {'unclassified' 'Alexandrium_doublet' 'Alexandrium_triplet' ...
-    'Alexandrium_quad' 'Azadinium' 'Beads' 'Boreadinium' 'bubbles' ...
-    'Centric<10' 'Detritus' 'DinoMix' 'FlagMix' 'Fragilariopsis' 'Helicotheca' ... 
+    'Alexandrium_quad' 'Azadinium' 'Beads' 'Boreadinium' ...
+    'Centric<10' 'DinoMix' 'FlagMix' 'Fragilariopsis' 'Helicotheca' ... 
     'Leptocylindrus' 'Lio_Thal' 'Navicula' 'Noctiluca' 'Paralia' 'Torodinium'...
-    'small_misc'};
+    'small_misc' 'Ash_dark' 'Ash_glassy'};
 
 compile_train_features_user_training(manualpath,feapath_base,maxn,minn,class2skip);
 
-%% Train (make) the classifier
-
+%% Step 2: Train (make) the classifier
 result_path = 'F:\IFCB104\manual\summary\'; %USER location of training file and classifier output
-train_filename = 'UserExample_Train_08May2018'; %USER what file contains your training features
+train_filename = 'UserExample_Train_22Jun2018'; %USER what file contains your training features
 result_str = 'UserExample_Trees_';
 nTrees = 100; %USER how many trees in your forest; choose enough to reach asymptotic error rate in "out-of-bag" classifications
 
 make_TreeBaggerClassifier_user_training(result_path, train_filename, result_str, nTrees)
 
-%%
+%% If want to remake figures related to classifier output
 classifier_oob_analysis('F:\IFCB104\manual\summary\UserExample_Trees_23Mar2018');
 
 %% Find the volume sampled in milliliters for >1 IFCB files
