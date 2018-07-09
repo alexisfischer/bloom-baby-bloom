a5a80dd3f3a17f3efcfc65ebfb9c2874d80428c7
afisch <afischer9@gmail.com>
2018-07-09 10:39:28 -0700

    new roms data

--- SCW/plotRAI.m
+++ SCW/plotRAI.m
@@ -1,23 +1,74 @@
-% resultpath = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\';
-% load([resultpath 'Data\RAI_SCW.mat']);
-
 resultpath = '~/Documents/MATLAB/bloom-baby-bloom/SCW/';
 load([resultpath 'Data/RAI_SCW.mat'],'r');
 load([resultpath 'Data/SCW_SCOOS.mat'],'a');
-load([resultpath 'Data/TempChlTurb_SCW'],'S');
-load([resultpath 'Data/Weatherstation_SCW'],'SC');
 
-%[r] = import_RAI();
-%[a] = import_SCOOS_weeklySCW();
+%% Chl threshold, color proportional to RAI score, and organize data in matrix
 
+dnchl=a.dn(find(a.chl>=3)); %find the dates with chlorophyll exceeding a threshold
+for h=1:length(r)
+    r(h).xChl=zeros(size(dnchl));
+    r(h).yChl=zeros(size(dnchl));
+    r(h).color=zeros(size(dnchl));    
+    for i=1:length(dnchl) %find corresponding dates in rai dataset
+        for j=1:length(r(h).dn)
+            if dnchl(i) == r(h).dn(j)
+                r(h).xChl(i) = r(h).dn(j); %only use data with Chl > a threshold
+                r(h).yChl(i) = r(h).count(j); %only use data with Chl > a threshold
+            else
+            end
+        end
+    end
+end
 
-%% Seasonality 2012-2019 dinoflagellates and diatoms with Chlorophyll Threshold
+%organize data into a year x week matrix
+for i=1:length(r) 
+    for j=1:length(r(i).yChl)
+        r(i).color(j) = r(i).yChl(j)./4; %Assign colors proporational to RAI score  
+    end
+    [~,r(i).y_mat,r(i).yearlist,~]=timeseries2ydmat(r(i).xChl,r(i).color); %takes a timeseries and makes it into a year x day matrix
+    [r(i).y_wkmat,~,r(i).yd_wk]=ydmat2weeklymat(r(i).y_mat,r(i).yearlist); %takes a year x day matrix and makes it a year x 1 week matrix
+end
+
+%% Pcolor RAI in a year x week matrix for each species
+ind=1; %USER enter your species
+%species=[55;50;36;34;32;28;19;17;7;5;1];
+
+figure('Units','inches','Position',[1 1 7 5],'PaperPositionMode','auto'); 
+X=[r(ind).yd_wk ;r(ind).yd_wk(end)+7]; %365 days in a year
+Y=[r(ind).yearlist(1:8) r(ind).yearlist(8)+1];
+C=[[r(ind).y_wkmat(:,1:8)'; zeros(1,52)] zeros(9,1)];
+
+pcolor(X,Y,C); caxis([0 1]); colormap(flipud(hot));
+datetick('x',4); axis square; shading flat;
+
+set(gca,'xticklabel',{'J','F','M','A','M','J','J','A','S','O','N','D',''},...
+    'ylim',[2012 2019],'ytick',2012:2019,'yticklabel',{'','2012','2013','2014',...
+    '2015','2016','2017','2018'},'fontsize',12,'tickdir','out');
+
+ylabel('Year', 'fontsize',14, 'fontweight','bold');
+xlabel('Month', 'fontsize',14, 'fontweight','bold');
+title(num2str(r(ind).name), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
+
+h=colorbar('Ticks',[0,.25,.5,.75,1],'TickLabels',...
+    {'0 Absent','1 Rare','2 Present','3 Common','4 Abundant'});
+h.Label.String = 'RAI (fraction)';
+h.FontSize = 12;
+h.Label.FontSize = 14;
+h.Label.FontWeight = 'bold';
+h.TickDirection = 'out';
+
+% set figure parameters
+set(gcf,'color','w');
+print(gcf,'-dtiff','-r600',[resultpath 'Figs\RAI_ChlThr_' num2str(r(ind).name) '_2012-2018.tif']);
+hold off
+   
+%% Pcolor Dino vs Diatoms in a year x week matrix
 clearvars class2do_string dnok dnchl dnk h i ii j rk rok y_mat y_wkmat yd_wk yearlist;
 
-%class2do_string = 'Diatoms'; ind=61;
- class2do_string = 'Dinoflagellates'; ind=60;
+%ind=61; %diatoms
+ind=60; %dinoflagellates
 
-i=find(a.chl>=3); dnchl=a.dn(i); %find the dates with chlorophyll exceeding a threshold
+dnchl=a.dn(find(a.chl>=3)); %find the dates with chlorophyll exceeding a threshold
 for i=1:length(dnchl) %find corresponding dates in rai dataset
     for j=1:length(r(ind).dn)
         if dnchl(i) == r(ind).dn(j)
@@ -30,165 +81,109 @@ end
 
 ii=find(rk); rok=rk(ii)'; dnok=dnk(ii)'; %remove NaNs
 [~,y_mat,yearlist,~]=timeseries2ydmat(dnok,rok); %takes a timeseries and makes it into a year x day matrix
+[y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist); %takes a year x day matrix and makes it a year x 2 week matrix
+
+figure('Units','inches','Position',[1 1 6.5 5.5],'PaperPositionMode','auto'); 
+X=[yd_wk]; 
+Y=[yearlist(1:8) yearlist(8)+1];
+C=[[y_wkmat(:,1:8)';zeros(1,52)]];
+
+pcolor(X,Y,C); caxis([0 0.7]); colormap(flipud(hot));
+datetick('x',4); axis square; shading flat;
+
+set(gca,'xticklabel',{'J','F','M','A','M','J','J','A','S','O','N','D',''},...
+    'ylim',[2012 2019],'ytick',2012:2019,'yticklabel',{'','2012','2013','2014',...
+    '2015','2016','2017','2018'},'fontsize',12,'tickdir','out');
+
+ylabel('Year', 'fontsize',14, 'fontweight','bold');
+xlabel('Month', 'fontsize',14, 'fontweight','bold');
+title(num2str(r(ind).name), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
+
+h=colorbar;
+h.Label.String = 'Fraction of RAI biomass';
+h.FontSize = 12;
+h.Label.FontSize = 14;
+h.Label.FontWeight = 'bold';
+h.TickDirection = 'out';
+
+% set figure parameters
+set(gcf,'color','w');
+print(gcf,'-dtiff','-r600',[resultpath 'Figs\RAI_ChlThr_' num2str(r(ind).name) '_2012-2018.tif']);
+hold off
+
+%% Pcolor RAI of select species in a year x week matrix 
+year='2018';
+species=[55;50;36;34;32;28;19;17;7;5;1];
+for i=1:length(species)
+    M(i,:)=r(species(i)).y_wkmat(:,8)';
+end
+name={r(species).name}
+
+figure('Units','inches','Position',[1 1 7 5],'PaperPositionMode','auto'); 
+X=[r(1).yd_wk ;r(1).yd_wk(end)+7]; %365 days in a year
+Y=[1:12];
+C=[[M; zeros(1,52)] zeros(12,1)];
+
+pcolor(X,Y,C); caxis([0 1]); colormap(flipud(hot));
+datetick('x',4); axis square; shading flat;
+
+set(gca,'xticklabel',{'J','F','M','A','M','J','J','A','S','O','N','D',''},...
+    'ytick',1:12,'yticklabel',{'',r(species).name},'fontsize',12,'tickdir','out');
+
+xlabel('Month', 'fontsize',14, 'fontweight','bold');
+title(num2str(year), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
+
+h=colorbar('Ticks',[0,.25,.5,.75,1],'TickLabels',...
+    {'0 Absent','1 Rare','2 Present','3 Common','4 Abundant'});
+h.Label.String = 'RAI (fraction)';
+h.FontSize = 12;
+h.Label.FontSize = 14;
+h.Label.FontWeight = 'bold';
+h.TickDirection = 'out';
+
+% set figure parameters
+set(gcf,'color','w');
+print(gcf,'-dtiff','-r600',[resultpath 'Figs\RAI_speces_ChlThr_' num2str(year) '.tif']);
+hold off
+
+ %%
+xmat=xmat';
+ydino=ydino';
+ydiat=ydiat';
+
+fx_dino=[ydino-[yAKA+yCER+yDINO+yLING+yPRO]]./ymat; %fraction other dinos
+fx_diat=[ydiat-[yCHAET+yC+yDETO+yPN+yEUC+yGUIN]]./ymat; %fraction other diatoms
+fx_other=[ymat-[ydino+ydiat]]./ymat; %fraction not dinos or diatoms
 
 figure;
-[y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist); %takes a year x day matrix and makes it a year x 2 week matrix
-pcolor([yd_wk ;yd_wk(end)+7],[yearlist(1:8) yearlist(8)+1],[[y_wkmat(:,1:8)';zeros(1,52)] zeros(9,1)]) %for just 2006:2014
-caxis([0 .7]) %RAI
-colormap(flipud(hot));
 
-ylabel('Year', 'fontsize',14, 'fontname', 'arial');
-xlabel('Month', 'fontsize',14, 'fontname', 'arial');
+bar(xmat,[yAKA./ymat yCER./ymat yDINO./ymat yLING./ymat yPRO./ymat...
+    yCHAET./ymat yDETO./ymat yEUC./ymat yGUIN./ymat yPN./ymat yC./ymat...
+    fx_dino fx_diat fx_other], 0.5, 'stack');
+ax = get(gca);
+cat = ax.Children;
 
-title(num2str(class2do_string), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
+cstr = [[220 220 220]/255; [110 110 110]/255; [0 0 0]/255;...
+    [255,255,153]/255; [166,206,227]/255; [31,120,180]/255;...
+    [178,223,138]/255; [51,160,44]/255; [251,154,153]/255; [227,26,28]/255;...
+    [253,191,111]/255; [255,127,0]/255; [202,178,214]/255;...
+    [106,61,154]/255;[220 220 220]/255];
 
-set(gca,'ylim',[2012 2019],'yticklabel',{'2012','2013','2014','2015','2016','2017','2018',''},'tickdir','out');
-h=colorbar;
-set(get(h,'ylabel'),'string','RAI (fraction)','fontsize',14,'fontname','arial');
-%set(get(h,'ylabel'),'string','RAI','fontsize',14,'fontname','arial');
- h.TickDirection = 'out';
-datetick('x',4)
-axis square
-shading flat
-
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs\Seasonality_RAI_ChlThr_' num2str(class2do_string) '_2012-2019.tif']);
-hold off
-
-%% Dino Diatom test 2018
-clearvars class2do_string dnok dnchl dnk h i ii j rk rok y_mat y_wkmat yd_wk yearlist;
-
-figure('Units','inches','Position',[1 1 8 5],'PaperPositionMode','auto');
-subplot = @(m,n,p) subtightplot (m, n, p, [0.05 0.05], [0.08 0.04], [0.09 0.04]);
-%subplot = @(m,n,p) subtightplot(m,n,p,opt{:}); 
-%where opt = {gap, width_h, width_w} describes the inner and outer spacings.
-
-subplot(4,1,1);
-class2do_string = 'Dinoflagellates';
-i=find(a.chl>=3); dnchl=a.dn(i); %find the dates with chlorophyll exceeding a threshold
-for i=1:length(dnchl) %find corresponding dates in rai dataset
-    for j=1:length(r(60).dn)
-        if dnchl(i) == r(60).dn(j)
-            dnk(i) = r(60).dn(j);
-            rk(i) = r(60).rai(j);           
-        else
-        end
-    end
+for ii = 1:length(cat)
+    set(cat(ii), 'FaceColor', cstr(ii,:),'BarWidth',1)
 end
-ii=find(rk); rok=rk(ii)'; dnok=dnk(ii)'; %remove NaNs
-[~,y_mat,yearlist,~]=timeseries2ydmat(dnok,rok);
-[y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist);
 
-pcolor(yd_wk(1:24),[1,2],[y_wkmat(1:24,8)'; zeros(1,24)]) %01jan-01jul2018
-caxis([0 0.7]) %RAI
-shading flat
-datetick('x',4)
-set(gca,'ylim',[1 2],'ytick',[1,2],'yticklabel',{},'xticklabel',{},'tickdir','out');
-ylabel('Dinoflagellate','fontsize',12, 'fontname', 'Arial');    
-colormap(flipud(hot));
-
-% h=colorbar;
-% set(get(h,'ylabel'),'string','fraction','fontsize',14,'fontname','arial');
-% h.TickDirection = 'out';
-% h.Location='NorthOutside';
+datetick('x', 3, 'keeplimits')
+xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
+ylim([0;1])
+set(gca,'xticklabel',{}, 'fontsize', 10, 'fontname', 'arial','tickdir','out')
+ylabel('Fraction of total biovolume', 'fontsize', 12, 'fontname', 'arial','fontweight','bold')
+h=legend('Akashiwo','Ceratium','Dinophysis','Lingulodinium','Prorocentrum',...
+    'Chaetoceros','Detonula','Eucampia','Guinardia','Pseudo-nitzschia','Centric diatoms',...
+    'other dinos','other diatoms','other cell-derived');
+set(h,'Position',[0.808159725831097 0.399956604207141 0.187499996391125 0.246744784681747]);
 hold on
-
-subplot(4,1,2)
-class2do_string = 'Diatoms';
-i=find(a.chl>=3); dnchl=a.dn(i); %find the dates with chlorophyll exceeding a threshold
-for i=1:length(dnchl) %find corresponding dates in rai dataset
-    for j=1:length(r(61).dn)
-        if dnchl(i) == r(61).dn(j)
-            dnk(i) = r(61).dn(j);
-            rk(i) = r(61).rai(j);           
-        else
-        end
-    end
-end
-ii=find(rk); rok=rk(ii)'; dnok=dnk(ii)'; %remove NaNs
-[~,y_mat,yearlist,~]=timeseries2ydmat(dnok,rok);
-[y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist);
-
-pcolor(yd_wk(1:24),[1,2],[y_wkmat(1:24,8)'; zeros(1,24)]) %01jan-01jul2018
-caxis([0 0.7]) %RAI
-shading flat
-datetick('x',4)
-set(gca,'ylim',[1 2],'ytick',[1,2],'yticklabel',{},'xticklabel',{},'tickdir','out');
-ylabel('Diatom','fontsize',12, 'fontname', 'Arial');    
-colormap(flipud(hot));
- 
-hold on
-
-subplot(4,1,3); %chlorophyll
-    plot(a.dn,(a.chl),'*','Color','k');
-    hold on
-    plot(S.dn,(S.chl),'-','Color','k');
-    xax1=datenum('2018-01-01'); xax2=datenum('2018-07-01');
-    set(gca,'xgrid', 'on','ylim',[0 20],'ytick',0:10:20,'xlim',[xax1 xax2],...
-        'xtick',[datenum('2018-01-01'),datenum('2018-02-01'),...
-        datenum('2018-03-01'),datenum('2018-04-01'),datenum('2018-05-01'),...
-        datenum('2018-06-01'),datenum('2018-07-01')],'Xticklabel',{},...
-        'tickdir','out');      
-    ylabel('Chl (mg m^{-3})','fontsize',12, 'fontname', 'Arial');    
-    hold on
-    
-subplot(4,1,4);
-[U,~]=plfilt(SC(7).U,SC(7).DN);
-[V,DN]=plfilt(SC(7).V,SC(7).DN);
-[~,u,~] = ts_aggregation(DN,U,1,'day',@mean);
-[time,v,~] = ts_aggregation(DN,V,1,'day',@mean);
-xax1=datenum('2018-01-01'); xax2=datenum('2018-07-01');
-yax1=-3; yax2=3;
-stick(time,u,v,xax1,xax2,yax1,yax2,' ');
-hold on
-set(gca,'xtick',[datenum('2018-01-01'),datenum('2018-02-01'),...
-    datenum('2018-03-01'),datenum('2018-04-01'),datenum('2018-05-01'),...
-    datenum('2018-06-01'),datenum('2018-07-01')],...
-    'Xticklabel',{'Jan','Feb','Mar','Apr','May','Jun','Jul'},'fontsize',12);
-
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs\Diatom_Dino_2018.tif']);
-hold off
-
-%% Plot wind pcolor panel plot
-
-time=[SC(1).DN;SC(2).DN;SC(3).DN;SC(4).DN;SC(5).DN;SC(6).DN;SC(7).DN];
-windspeed=[SC(1).WINDSPEED;SC(2).WINDSPEED;SC(3).WINDSPEED;SC(4).WINDSPEED;...
-    SC(5).WINDSPEED;SC(6).WINDSPEED;SC(7).WINDSPEED];
-[dn,wsp,~] = ts_aggregation(time,windspeed,1,'day',@mean);
-
-param_string = 'Windspeed';
-ii=find(wsp); rok=wsp(ii); dnok=dn(ii); %remove NaNs
-
-[~,y_mat,yearlist,~]=timeseries2ydmat(dnok,rok); %takes a timeseries and makes it into a year x day matrix
-
-figure;
-[y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist); %takes a year x day matrix and makes it a year x 2 week matrix
-pcolor([yd_wk ;yd_wk(end)+7],[yearlist(1:7) yearlist(7)+1],[[y_wkmat(:,1:7)';zeros(1,52)] zeros(8,1)]) %for just 2006:2014
-caxis([0 5]) 
-
-colormap(flipud(hot));
-
-ylabel('Year', 'fontsize',14, 'fontname', 'arial');
-xlabel('Month', 'fontsize',14, 'fontname', 'arial');
-
-title(num2str(param_string), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
-
-set(gca,'ylim',[2012 2019],'tickdir','out');
-h=colorbar;
-set(get(h,'ylabel'),'string','m s^{-1}','fontsize',14,'fontname','arial');
- h.TickDirection = 'out';
-datetick('x',4)
-axis square
-shading flat
-
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs\Seasonality_' num2str(param_string) '_2012-2019.tif']);
-hold off
-
+  
 %% Seasonality 2012-2019 Chlorophyll, Temperature, Nitrate, Silicate
 
 % param_string = 'Silicate';
@@ -197,72 +192,49 @@ hold off
 % param_string = 'Nitrate';
 % ii=find(a.NitrateuM); rok=a.NitrateuM(ii); dnok=a.dn(ii); %remove NaNs
 
-% param_string = 'Temperature';
-% ii=find(a.temp); rok=a.temp(ii); dnok=a.dn(ii); %remove NaNs
+param_string = 'Temperature';
+ii=find(a.temp); rok=a.temp(ii); dnok=a.dn(ii); %remove NaNs
 
-param_string = 'Chlorophyll';
-ii=find(a.chl); rok=a.chl(ii); dnok=a.dn(ii); %remove NaNs
+% param_string = 'Chlorophyll';
+% ii=find(a.chl); rok=a.chl(ii); dnok=a.dn(ii); %remove NaNs
 
 [~,y_mat,yearlist,~]=timeseries2ydmat(dnok,rok); %takes a timeseries and makes it into a year x day matrix
-
-figure;
 [y_wkmat,~,yd_wk]=ydmat2weeklymat(y_mat,yearlist); %takes a year x day matrix and makes it a year x 2 week matrix
-pcolor([yd_wk ;yd_wk(end)+7],[yearlist(1:8) yearlist(8)+1],[[y_wkmat(:,1:8)';zeros(1,52)] zeros(9,1)]) %for just 2006:2014
+
+figure('Units','inches','Position',[1 1 6.5 5.5],'PaperPositionMode','auto'); 
+X=[yd_wk]; 
+Y=[yearlist(1:8) yearlist(8)+1];
+C=[y_wkmat(:,1:8)';zeros(1,52)];
+
+pcolor(X,Y,C); caxis([0 0.7]); colormap(flipud(hot));
 %caxis([0 50]) %Silicate
 %caxis([0 20]) %Nitrate
-%caxis([11 18]) %Temperature
-caxis([0 30]) %Chlorophyll
+caxis([11 18]) %Temperature
+%caxis([0 20]) %Chlorophyll
 
 colormap(flipud(hot));
+datetick('x',4); axis square; shading flat;
 
-ylabel('Year', 'fontsize',14, 'fontname', 'arial');
-xlabel('Month', 'fontsize',14, 'fontname', 'arial');
+set(gca,'xticklabel',{'J','F','M','A','M','J','J','A','S','O','N','D',''},...
+    'ylim',[2012 2019],'ytick',2012:2019,'yticklabel',{'','2012','2013','2014',...
+    '2015','2016','2017','2018'},'fontsize',12,'tickdir','out');
 
+ylabel('Year', 'fontsize',14, 'fontweight','bold');
+xlabel('Month', 'fontsize',14, 'fontweight','bold');
 title(num2str(param_string), 'fontsize',16, 'fontname', 'arial','fontweight','bold');
 
-set(gca,'ylim',[2012 2019],'yticklabel',{'2012','2013','2014','2015','2016','2017','2018',''},'tickdir','out');
 h=colorbar;
-%set(get(h,'ylabel'),'string','uM','fontsize',14,'fontname','arial');
-%set(get(h,'ylabel'),'string','^oC','fontsize',14,'fontname','arial');
-set(get(h,'ylabel'),'string','mg m^{-3}','fontsize',14,'fontname','arial');
- h.TickDirection = 'out';
-datetick('x',4)
-axis square
-shading flat
+h.Label.String = 'mg m^{-3}';
+% h.Label.String = 'uM';
+% h.Label.String = '^oC';
+h.FontSize = 12;
+h.Label.FontSize = 14;
+h.Label.FontWeight = 'bold';
+h.TickDirection = 'out';
 
 % set figure parameters
 set(gcf,'color','w');
 print(gcf,'-dtiff','-r600',[resultpath 'Figs\Seasonality_' num2str(param_string) '_2012-2019.tif']);
-hold off
-
-%% Seasonality 2016 all species
-figure('Units','inches','Position',[1 1 6 5],'PaperPositionMode','auto');        
-
-for i=1:13
-    A=r(i).rai; ii=find(A); Aok=A(ii); y = Aok;
-    x = r(i).dn(ii);
-    [ mdate_mat, y_mat, yearlist, yd ] = timeseries2ydmat(x,y); %takes a timeseries and makes it into a year x day matrix
-    [ y_wkmat, mdate_wkmat, yd_wk ] = ydmat2weeklymat( y_mat, yearlist ); %takes a year x day matrix and makes it a year x 1 week matrix
-    pcolor([yd_wk ;yd_wk(end)+7],[i i+1],[[y_wkmat(:,10)';zeros(1,52)] zeros(2,1)]); 
-    hold on
-end
-
-caxis([0 0.6])
-colormap(flipud(hot));
-xlabel('Month', 'fontsize',14, 'fontname', 'arial');
-
-set(gca,'ylim',[1 14],'ytick',1:1:14,'fontsize',13,'tickdir','out',...
-    'YTickLabel',[' ',flipud({r(1:13).name})]);
-h=colorbar;
-set(get(h,'ylabel'),'string','RAI','fontsize',13,'fontname','arial');
- h.TickDirection = 'out';
-datetick('x',4)
-axis square
-shading flat
-
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs\Seasonality_RAI_group_compare_2016.tif']);
 hold off
 
 
@@ -332,3 +304,29 @@ set(gca,'xgrid', 'on','ylim',[14 16],'ytick',14:1:16,...
 set(gcf,'color','w');
 print(gcf,'-dtiff','-r600',[resultpath 'Figs\RAI_Dino_Diat_2007-2018.tif']);
 hold off
+
+
+%% Script to assign colors based off of specific values
+% color=flipud(autumn); 
+% colormap(color);    
+% d=[0;1;2;3;4]/4;
+% dcol=zeros(length(d),3); %preallocate space   
+% for i=1:length(dcol)  
+%     dcol(i,:)=color(round(d(i)*length(color)),:);
+% end       
+
+% %set colors for all classes    
+% for i=1:length(r) 
+%     r(i).color=ones(length(r(i).count),3); %default white for 0
+%     for j=1:length(r(i).count)
+%         if r(i).count(j) == 1
+%             r(i).color(j,:) = dcol(1,:);           
+%         elseif r(i).count(j) == 2
+%             r(i).color(j,:) = dcol(2,:);   
+%         elseif r(i).count(j) == 3
+%             r(i).color(j,:) = dcol(3,:);    
+%         elseif r(i).count(j) == 4
+%             r(i).color(j,:) = dcol(4,:);            
+%         end
+%     end
+% end
