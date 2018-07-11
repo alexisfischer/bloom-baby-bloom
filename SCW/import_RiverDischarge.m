3e4133f437deeec4a331aa592a7443ba19d2c78d
afisch <afischer9@gmail.com>
2018-07-11 14:27:29 -0700

    new pajaro river

--- SCW/import_RiverDischarge.m
+++ SCW/import_RiverDischarge.m
@@ -2,7 +2,7 @@
 % Discharge (cubic feet per second)
 % Date,  dn=datenum(TimeUTC,'yyyy-mm-dd HH:MM');
 
-filename = '/Users/afischer/Documents/MATLAB/bloom-baby-bloom/SCW/Data/PajaroRiver_2012-2018.txt';
+filename = '/Users/afischer/Documents/MATLAB/bloom-baby-bloom/SCW/Data/PajaroRiver_1993-2018.txt';
 delimiter = {'\t',' ',':'};
 startRow = 31;
 formatSpec = '%*q%*q%{yyyy-MM-dd HH:mm}D%*q%f%*s%[^\n\r]';
@@ -14,19 +14,24 @@ fclose(fileID);
 dt = dataArray{:, 1};
 discharge = dataArray{:, 2};
 
+[paj.dn,paj.dis,~] = ts_aggregation(datenum(dt),discharge,1,'day',@mean);
+
 
 %% plot Pajaro River discharge
-figure('Units','inches','Position',[1 1 7 2.5],'PaperPositionMode','auto');
+load PajaroRiver;
 
-plot(datenum(dt),discharge,'b-','Linewidth',1);
+figure('Units','inches','Position',[1 1 8 2],'PaperPositionMode','auto');
+
+plot(paj.dn,paj.dis,'b-','Linewidth',1);
 datetick('x','yyyy');
-set(gca,'yscale','log','ylim',[1 10^4],...
-    'xlim',[datenum('2012-01-01') datenum('2019-01-01')],...     
+%'yscale','log',
+set(gca,...
+    'xlim',[datenum('1993-01-01') datenum('2019-01-01')],...     
 	'fontsize',10,'XAxisLocation','bottom','TickDir','out'); 
 ylabel('Discharge (ft^3 sec^-^1)','fontsize',11,'fontweight','bold');        
 hold on
 
 % set figure parameters
 set(gcf,'color','w');
-print(gcf,'-dtiff','-r600','~/Documents/MATLAB/bloom-baby-bloom/SCW/Figs/PajaroDischarge_2012-2018.tif')
+print(gcf,'-dtiff','-r600','~/Documents/MATLAB/bloom-baby-bloom/SCW/Figs/PajaroDischarge_1993-2018.tif')
 hold off 
