8e05d3a4d47340e082195f1e4d36154095b43bba
afisch <afischer9@gmail.com>
2018-08-02 15:14:03 -0700

    incorporated MLD code into biovolume

--- SCW/import_CAROMS.m
+++ SCW/import_CAROMS.m
@@ -1,3 +1,4 @@
+%% import ROMS data for particular years of interest
 
 year= 2017;
 
@@ -7,6 +8,8 @@ in_dir='http://west.rssoffice.com:8080/thredds/dodsC/roms/CA3km-nowcast/MB/';
 out_dir=['~/Documents/MATLAB/bloom-baby-bloom/SCW/Data/ROMS/MB_temp_sal_' num2str(year) '']; %change for whatever year
 in_dir_base='ca_subMB_das_';
 
+%%
+% organize data of interest
 for i=1:length(filename)
     disp(filename(i,1:26));
     indir_name = [in_dir filename(i,1:26)];
@@ -30,7 +33,6 @@ ROMS(i).S=squeeze(salt(54,49,:));
 
 end
 
-%%
 % remove bad data on '11-Apr-2018 09:00:00'
 %ROMS(392) = [];
 
@@ -46,7 +48,7 @@ for i=1:length(ROMS)
     
 end
 
-%find where dT from the surface exceeds 0.5ºC, aka the MLD
+% find where dT from the surface exceeds 0.5ºC, aka the MLD
 for i=1:length(ROMS)
     for j=1:length(ROMS(i).Ti)
        ROMS(i).diff(j)=abs(diff([ROMS(i).Ti(1) ROMS(i).Ti(j)]));
@@ -58,156 +60,19 @@ for i=1:length(ROMS)
     ROMS(i).diff=ROMS(i).diff';
 end
 
-out_dir=['~/Documents/MATLAB/bloom-baby-bloom/SCW/Data/ROMS/MB_temp_sal_' num2str(year) '']; %change for whatever year
+out_dir=['~/Documents/MATLAB/bloom-baby-bloom/SCW/Data/ROMS/MB_temp_sal_' num2str(year) '']; 
 save(out_dir,'ROMS');
 
 
-%% plot pcolor temperature and dTdz profile
+%% notes
 
-year=2017;
-resultpath='~/Documents/MATLAB/bloom-baby-bloom/SCW/';
-load(['Data/ROMS/MB_temp_sal_' num2str(year) ''],'ROMS');
-figure('Units','inches','Position',[1 1 8 4],'PaperPositionMode','auto');
- subplot = @(m,n,p) subtightplot (m, n, p, [0.06 0.06], [0.08 0.04], [0.09 0.2]);
-% %subplot = @(m,n,p) subtightplot(m,n,p,opt{:}); 
-% %where opt = {gap, width_h, width_w} describes the inner and outer spacings. 
-
-subplot(2,1,1);
-    X=[ROMS.dn]';
-    Y=[ROMS(1).Zi]';
-    C=[ROMS.Ti];
-    pcolor(X,Y,C); shading interp;
-    caxis([10 16]); datetick('x',4);  grid on; 
-    hold on
-    xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
-    set(gca,'xticklabel',{},'Ydir','reverse','ylim',[0 40],'ytick',0:10:40,...
-        'fontsize',10,'tickdir','out');
-    ylabel('Depth (m)','fontsize',12,'fontweight','bold');
-    h=colorbar; 
-    h.FontSize = 10;
-    h.Label.String = 'T (^oC)'; 
-    h.Label.FontSize = 12;
-    h.Label.FontWeight = 'bold';
-    h.TickDirection = 'out';
-    hold on
-    
-subplot(2,1,2);
-    X=[ROMS.dn]';
-    Y=[ROMS(1).Zi]';
-    C=[ROMS.diff];
-    pcolor(X,Y,C); shading interp;
-    caxis([0 0.5]); datetick('x',4);  grid on; 
-    hold on
-    hh=plot(X,smooth([ROMS.mld5],20),'k-','linewidth',3);
-    hold on
-    xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))    
-    set(gca,'Ydir','reverse','ylim',[0 40],'ytick',0:10:40,'fontsize',10,'tickdir','out');
-    datetick('x','mmm','keeplimits','keepticks');
-    ylabel('Depth (m)','fontsize',12,'fontweight','bold');
-    h=colorbar; 
-    h.Label.String = '\DeltaT from 0m (^oC)';
-    h.FontSize = 10;
-    h.Label.FontSize = 12;
-    h.Label.FontWeight = 'bold';
-    h.TickDirection = 'out';
-    hold on
-    legend([hh(1)],'\DeltaT = 0.5^oC','Location','SE');
-        
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs/ROMS_MB_Temp_MLD_' num2str(year) '.tif']);
-hold off
-
-%% plot pcolor temperature and dTdz profile
-
-year=2018;
-resultpath='~/Documents/MATLAB/bloom-baby-bloom/SCW/';
-load(['Data/ROMS/MB_temp_sal_' num2str(year) ''],'ROMS');
-figure('Units','inches','Position',[1 1 8 6],'PaperPositionMode','auto');
-subplot = @(m,n,p) subtightplot (m, n, p, [0.06 0.06], [0.08 0.04], [0.09 0.2]);
-%subplot = @(m,n,p) subtightplot(m,n,p,opt{:}); 
-%where opt = {gap, width_h, width_w} describes the inner and outer spacings. 
-
-subplot(3,1,1);
-    X=[ROMS.dn]';
-    Y=[ROMS(1).Zi]';
-    C=[ROMS.Ti];
-
-    pcolor(X,Y,C); shading interp;
-    caxis([10 16]); datetick('x',4);  grid on; 
-    hold on
-    xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))
-    set(gca,'xticklabel',{},'Ydir','reverse','ylim',[0 40],'ytick',0:20:40,'fontsize',10,'tickdir','out');
-    ylabel('Depth (m)','fontsize',12,'fontweight','bold');
-    h=colorbar; 
-    h.FontSize = 10;
-    h.Label.String = 'T (^oC)'; 
-    h.Label.FontSize = 12;
-    h.Label.FontWeight = 'bold';
-    h.TickDirection = 'out';
-    hold on
-
-subplot(3,1,2);
-    X=[ROMS.dn]';
-    Y=[ROMS(1).Zi(1:40)]';
-    C=[ROMS.dTdz];
-    pcolor(X,Y,C); shading interp;
-    caxis([0 0.2]); datetick('x',4);  grid on; 
-    hold on
-    plot(X,smooth([ROMS.Zmax],20),'w-','linewidth',3);
-    hold on
-    xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))    
-    set(gca,'Ydir','reverse','ylim',[0 40],'ytick',0:20:40,'fontsize',10,...
-        'xticklabel',{},'tickdir','out');
-    ylabel('Depth (m)','fontsize',12,'fontweight','bold');
-    h=colorbar; 
-    h.Label.String = 'dTdz (^oC m^{-1})';
-    h.FontSize = 10;
-    h.Label.FontSize = 12;
-    h.Label.FontWeight = 'bold';
-    h.TickDirection = 'out';
-    hold on
-
-subplot(3,1,3);
-    X=[ROMS.dn]';
-    Y=[ROMS(1).Zi]';
-    C=[ROMS.diff];
-    pcolor(X,Y,C); shading interp;
-    caxis([0 0.5]); datetick('x',4);  grid on; 
-    hold on
-    hh=plot(X,smooth([ROMS.mld2],20),'r-',X,smooth([ROMS.mld5],20),'k-','linewidth',3);
-    hold on
-    xlim(datenum([['01-Jan-' num2str(year) '']; ['01-Jul-' num2str(year) '']]))    
-    set(gca,'Ydir','reverse','ylim',[0 40],'ytick',0:10:40,'fontsize',10,'tickdir','out');
-    datetick('x','mmm','keeplimits','keepticks');
-    ylabel('Depth (m)','fontsize',12,'fontweight','bold');
-    h=colorbar; 
-    h.Label.String = '\DeltaT from 0m (^oC)';
-    h.FontSize = 10;
-    h.Label.FontSize = 12;
-    h.Label.FontWeight = 'bold';
-    h.TickDirection = 'out';
-    hold on
-    legend([hh(1),hh(2)],'\DeltaT = 0.2^oC','\DeltaT = 0.5^oC','Location','SE');
-    
-% set figure parameters
-set(gcf,'color','w');
-print(gcf,'-dtiff','-r600',[resultpath 'Figs/ROMS_MB_Temp_dTdz_' num2str(year) '.tif']);
-hold off
-
-
-%%
 surface=zeros(length(ROMS),1);
 for i=1:length(ROMS)
     surface(i)=ROMS(i).temp(1,1);
 end
 
-%%
-
-
 imagesc(temp(:,:,1)) %surface, kinda like pcolor
 imagesc(salt(:,:,1)) %below, kinda like pcolor
-
 
 % daydir_0 = datetime({'2018010103'},'InputFormat','yyyyMMddHH'); % 01 January
 % daydir_end = datetime({'2018070103'},'InputFormat','yyyyMMddHH'); % 01 July
