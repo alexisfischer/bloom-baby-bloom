602d013309367f13f3b10712dc446b8894a40a6f
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2019-08-15 18:06:43 +0000

    new biovolume file

--- train_build_classifier.m
+++ train_build_classifier.m
@@ -8,17 +8,17 @@ maxn = 5000; %maximum number of images per class to include
 minn = 330; %minimum number for inclusion
 class2skip = {'Beads' 'Detritus' 'bubbles' 'Ash_dark' 'Ash_glassy'...
     'Centric<10' 'unclassified' 'Pollen' 'Clusterflagellate'...
-    'DinoMix' 'FlagMix' 'Leptocylindrus' 'zooplankton_misc'};
+    'DinoMix' 'FlagMix' 'Leptocylindrus' 'zooplankton_misc'...
+    'Ciliates' 'Strombidium' 'Mesodinium' 'Tiarina' 'Tintinnid' 'Tontonia'};
 
-class2group = {{'NanoP_less10' 'Cryptophyte' 'small_misc'}...
-        {'Ciliates' 'Strombidium' 'Mesodinium' 'Tiarina' 'Tintinnid' 'Tontonia'}};
+class2group = {'NanoP_less10' 'Cryptophyte' 'small_misc'};
 
 compile_train_features_user_training(manualpath,feapath_base,maxn,minn,class2skip,class2group);
 addpath(genpath('F:\IFCB104\manual\summary\')); % add new data to search path
 
 % Step 2: Train (make) the classifier
 result_path = 'F:\IFCB104\manual\summary\'; %USER location of training file and classifier output
-train_filename = 'UserExample_Train_24Jul2019'; %USER what file contains your training features
+train_filename = 'UserExample_Train_15Aug2019'; %USER what file contains your training features
 result_str = 'UserExample_Trees_';
 nTrees = 100; %USER how many trees in your forest; choose enough to reach asymptotic error rate in "out-of-bag" classifications
 
