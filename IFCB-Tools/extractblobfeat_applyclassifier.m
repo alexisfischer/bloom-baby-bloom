eb2572ddf8e13b4f3c538ba791ad9cd7741acdf9
Alexis Fischer <afischer9@gmail.com>
2021-09-16 15:46:24 -0700

    biovolume summary file

--- IFCB-Tools/extractblobfeat_applyclassifier.m
+++ IFCB-Tools/extractblobfeat_applyclassifier.m
@@ -3,12 +3,12 @@
 clear;
 
 %%%% modify according to dataset
-ifcbdir='D:\Shimada\'; 
-%ifcbdir='D:\BuddInlet\'; 
+%ifcbdir='D:\Shimada\'; 
+ifcbdir='D:\BuddInlet\'; 
 %ifcbdir='D:\SCW\'; 
 
-summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\Shimada\';
-%summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\BuddInlet\';
+%summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\Shimada\';
+summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\BuddInlet\';
 addpath(genpath(summarydir));
 
 classifier = 'D:\Shimada\classifier\summary\Trees_14Sep2021';
@@ -21,19 +21,19 @@ addpath(genpath('C:\Users\ifcbuser\Documents\GitHub\'));
 addpath(genpath([ifcbdir 'raw\']));
 sort_data_into_folders([ifcbdir 'raw\'],[ifcbdir 'data\2021\']);
 
-%% Step 2: Extract blobs
-addpath(genpath([ifcbdir 'data\2019\']));
+% Step 2: Extract blobs
+addpath(genpath([ifcbdir 'data\2021\']));
 addpath(genpath('C:\Users\ifcbuser\Documents\GitHub\'));
-start_blob_batch_user_training([ifcbdir 'data\2019\'],[ifcbdir 'blobs\2019\'],true);
+start_blob_batch_user_training([ifcbdir 'data\2021\'],[ifcbdir 'blobs\2021\'],true);
 
 % Step 3: Extract features
-addpath(genpath([ifcbdir 'blobs\2019\']));
-addpath(genpath([ifcbdir 'data\2019\']));
-start_feature_batch_user_training([ifcbdir 'data\2019\'],[ifcbdir 'blobs\2021\'],[ifcbdir 'features\2021\'],true)
+addpath(genpath([ifcbdir 'blobs\2021\']));
+addpath(genpath([ifcbdir 'data\2021\']));
+start_feature_batch_user_training([ifcbdir 'data\2021\'],[ifcbdir 'blobs\2021\'],[ifcbdir 'features\2021\'],true)
 
 % Step 4: Apply classifier
-addpath(genpath([ifcbdir 'features\2019\']));
-start_classify_batch_user_training(classifier,[ifcbdir 'features\2019\'],[ifcbdir 'class\class2019_v1\']);
+addpath(genpath([ifcbdir 'features\2021\']));
+start_classify_batch_user_training(classifier,[ifcbdir 'features\2021\'],[ifcbdir 'class\class2021_v1\']);
 
 
 %% Step 5: Summarize manual and classifier results for cell counts
@@ -47,4 +47,11 @@ summarize_cells_from_classifier([ifcbdir 'class\classXXXX_v1\'],...
 %% Step 6: Summarize manual and classifier results for biovolume
 summarize_biovol_eqdiam_from_manual([ifcbdir 'manual\'],[summarydir 'manual\'],...
     [ifcbdir 'data\'],[ifcbdir 'features\2019\'],2019,1/3.4)
+
+classpath_generic = [ifcbdir 'class\classxxxx_v1\'];
+feapath_generic = [ifcbdir 'features\xxxx\']; %Put in your featurepath byyear
+roibasepath_generic = [ifcbdir 'data\xxxx\']; %location of raw data
+yrrange = 2019;
+adhocthresh = 0.5;
+summarize_biovol_from_classifier([summarydir 'class\'],classpath_generic,feapath_generic,roibasepath_generic,adhocthresh,yrrange)
 
