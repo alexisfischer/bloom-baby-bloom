32f804c0b3a49f69e265e6ba4b09c75ed2794fac
Alexis Fischer <afischer9@gmail.com>
2022-01-06 12:26:02 -0800

    new summary file

--- IFCB-Tools/extractblobfeat_applyclassifier.m
+++ IFCB-Tools/extractblobfeat_applyclassifier.m
@@ -3,12 +3,12 @@
 clear;
 
 %%%% modify according to dataset
-ifcbdir='D:\Shimada\'; 
-%ifcbdir='D:\BuddInlet\'; 
+%ifcbdir='D:\Shimada\'; 
+ifcbdir='D:\BuddInlet\'; 
 %ifcbdir='D:\SCW\'; 
 
-summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\Shimada\';
-%summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\BuddInlet\';
+%summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\Shimada\';
+summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\BuddInlet\';
 addpath(genpath(summarydir));
 
 classifier = 'D:\Shimada\classifier\summary\Trees_04Jan2022';
@@ -32,8 +32,6 @@ addpath(genpath([ifcbdir 'data\2021\']));
 start_feature_batch_user_training([ifcbdir 'data\2021\'],[ifcbdir 'blobs\2021\'],[ifcbdir 'features\2021\'],true)
 
 %% Step 4: Apply classifier
-addpath(genpath([ifcbdir 'features\2019\']));
-start_classify_batch_user_training(classifier,[ifcbdir 'features\2019\'],[ifcbdir 'class\class2019_v1\']);
 addpath(genpath([ifcbdir 'features\2021\']));
 start_classify_batch_user_training(classifier,[ifcbdir 'features\2021\'],[ifcbdir 'class\class2021_v1\']);
 
@@ -44,7 +42,7 @@ start_classify_batch_user_training(classifier,[ifcbdir 'features\2021\'],[ifcbdi
 %   summarize_biovol_eqdiam_from_manual([ifcbdir 'manual\'],[summarydir 'manual\'],...
 %       [ifcbdir 'data\'],[ifcbdir 'features\2021\'],2021,1/3.4)
   
-%% Step 6: Summarize classifier results for biovolume and cells
+% Step 6: Summarize classifier results for biovolume and cells
 addpath(genpath([ifcbdir 'data\2021\']));
 addpath(genpath([ifcbdir 'features\2021\']));
 addpath(genpath([ifcbdir 'class\class2021_v1\']));
@@ -57,10 +55,8 @@ yrrange = 2021;
 adhocthresh = 0.5;
 
 summarize_biovol_from_classifier(sumdir,classpath_generic,feapath_generic,roibasepath_generic,adhocthresh,yrrange)
-summarize_biovol_from_classifier(sumdir,classpath_generic,feapath_generic,roibasepath_generic,adhocthresh,2019)
 
-summarize_cells_from_classifier(classpath_generic,[ifcbdir 'data\'],sumdir,2019); %you will need to do this separately for each year of data
-summarize_cells_from_classifier(classpath_generic,[ifcbdir 'data\'],sumdir,2021); %you will need to do this separately for each year of data
+summarize_cells_from_classifier(classpath_generic,[ifcbdir 'data\'],sumdir,yrrange); %you will need to do this separately for each year of data
 
 %% Adjust annotations with added class
 start_mc_adjust_classes_user_training('D:\Shimada\config\class2use_8','D:\Shimada\manual\')
