aba1fb1fee51b5e67bbedc4199a6c8cab106f615
afisch <afischer9@gmail.com>
2022-01-18 17:04:34 -0800

    Dinophysis

--- NOAA/BuddInlet/plot_manual_vs_class_Dinophysis.m
+++ NOAA/BuddInlet/plot_manual_vs_class_Dinophysis.m
@@ -1,6 +1,6 @@
 % plot Dinophysis in Budd Inlet
-addpath(genpath('~/MATLAB/ifcb-analysis/')); % add new data to search path
-addpath(genpath('~/MATLAB/bloom-baby-bloom/')); % add new data to search path
+addpath(genpath('~/Documents/MATLAB/ifcb-analysis/')); % add new data to search path
+addpath(genpath('~/Documents/MATLAB/bloom-baby-bloom/')); % add new data to search path
 clear;
 fprint=0;
 
@@ -8,7 +8,7 @@ filepath = '~/Documents/MATLAB/bloom-baby-bloom/';
 load([filepath 'IFCB-Data/BuddInlet/manual/count_class_manual_04Jan2022'],...
     'class2use','ml_analyzed','matdate','classcount');
 load([filepath 'IFCB-Data/BuddInlet/class/summary_biovol_allTB2021'],...
-    'class2useTB','ml_analyzedTB','mdateTB','classcountTB');
+    'class2useTB','ml_analyzedTB','mdateTB','classcountTB','classcountTB_above_optthresh');
 load([filepath 'NOAA/BuddInlet/Data/DinophysisMicroscopy'],'T');
 addpath(genpath(filepath));
 addpath(genpath('~/Documents/MATLAB/ifcb-analysis/')); 
@@ -16,13 +16,13 @@ addpath(genpath('~/Documents/MATLAB/ifcb-analysis/'));
 dtTB=datetime(mdateTB,'ConvertFrom','datenum');
 dt=datetime(matdate,'ConvertFrom','datenum');
 
-%% plot Dinophysis manual vs classifier
+% plot Dinophysis manual vs classifier
 figure('Units','inches','Position',[1 1 3.5 2],'PaperPositionMode','auto'); 
 
 xax1=datetime('2021-08-01'); xax2=datetime('2021-11-20');     
 
 idc=(strcmp('D_acuminata,D_acuta,D_caudata,D_fortii,D_norvegica,D_odiosa,D_parva,D_rotundata,D_tripos,Dinophysis',class2useTB));
-classifier=(classcountTB(:,idc)./ml_analyzedTB);
+classifier=(classcountTB_above_optthresh(:,idc)./ml_analyzedTB);
 idm=find(ismember(class2use,{'D_acuminata' 'D_acuta' 'D_caudata' 'D_fortii'...
     'D_norvegica' 'D_odiosa' 'D_parva' 'D_rotundata' 'D_tripos' 'Dinophysis'}));
 manual=sum(classcount(:,idm),2)./ml_analyzed;
