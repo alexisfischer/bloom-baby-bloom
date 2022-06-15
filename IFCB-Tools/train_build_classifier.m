8157e1b812fd5d7833da410a5f5c589cab5de4f6
Alexis Fischer <afischer9@gmail.com>
2022-06-15 14:18:43 -0700

    new summary files

--- IFCB-Tools/train_build_classifier.m
+++ IFCB-Tools/train_build_classifier.m
@@ -42,7 +42,7 @@ manualpath = 'D:\general\classifier\manual_merged\'; % manual annotation file lo
 feapath_base = 'D:\general\classifier\features_merged\'; %feature file location, assumes \yyyy\ organization
 outpath = 'D:\general\classifier\summary\'; % location to save training set
 maxn = 6000; %maximum number of images per class to include
-minn = 2000; %minimum number for inclusion
+minn = 1000; %minimum number for inclusion
 class2group={{'Dinophysis' 'Dinophysis_acuminata' 'Dinophysis_acuta' 'Dinophysis_caudata'...
         'Dinophysis_fortii' 'Dinophysis_norvegica' 'Dinophysis_odiosa' ...
         'Dinophysis_parva' 'Dinophysis_rotundata' 'Dinophysis_tripos'}...
@@ -50,15 +50,16 @@ class2group={{'Dinophysis' 'Dinophysis_acuminata' 'Dinophysis_acuta' 'Dinophysis
         'Pseudo-nitzschia_large_wide' 'Pseudo-nitzschia_small'}...
         {'Thalassiosira_chain' 'Thalassiosira_single'}...
         {'Chaetoceros_chain' 'Chaetoceros_single'}};
-IFCB='UCSC';
+%IFCB='UCSC';
+IFCB=[];
 
 compile_train_features_NWFSC(manualpath,feapath_base,outpath,maxn,minn,class2useName,class2skip,class2group,IFCB);
 addpath(genpath(outpath)); % add new data to search path
 
 % Step 3: Train (make) the classifier
 result_path = 'D:\general\classifier\summary\'; %USER location of training file and classifier output
-train_filename = 'Train_14Jun2022'; %USER what file contains your training features
-result_filename = 'Trees_14Jun2022';
+train_filename = 'Train_15Jun2022'; %USER what file contains your training features
+result_filename = 'Trees_15Jun2022';
 nTrees = 100; %USER how many trees in your forest; choose enough to reach asymptotic error rate in "out-of-bag" classifications
 make_TreeBaggerClassifier(result_path, train_filename, result_filename, nTrees)
 
