4c07ea94ee1018c8080f726793ebf87ae87aa7b6
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2023-02-13 21:27:48 -0800

    updated threshold tuning

--- IFCB-Tools/threshold/threshold_tuning.m
+++ IFCB-Tools/threshold/threshold_tuning.m
@@ -6,12 +6,19 @@ summarydir='C:\Users\ifcbuser\Documents\GitHub\bloom-baby-bloom\IFCB-Data\Shimad
 addpath(genpath(ifcbdir));
 addpath(genpath(summarydir));
 yrrange = 2019:2021;
-classifiername='CCS_v6';
+classifiername='CCS_v9';
 
-%% Step 1) Make a biovolume summary file from manual results
-summarize_biovol_from_manual([ifcbdir 'manual\'],[summarydir 'manual\'],[ifcbdir 'data\'],[ifcbdir 'features\'],1/3.4)
- 
-%% Step 2) Make summary file of counts for thresholds 0.1 to 1 for all classes
+classifier='D:\general\classifier\summary\Trees_CCS_v9';
+yr='2019';
+start_classify_batch_user_training(classifier,[ifcbdir 'features\' yr '\'],[ifcbdir 'class\class' yr '_v1\']);
+yr='2021';
+start_classify_batch_user_training(classifier,[ifcbdir 'features\' yr '\'],[ifcbdir 'class\class' yr '_v1\']);
+
+
+% %% Step 1) Make a biovolume summary file from manual results
+% summarize_biovol_from_manual([ifcbdir 'manual\'],[summarydir 'manual\'],[ifcbdir 'data\'],[ifcbdir 'features\'],1/3.4)
+%  
+% %% Step 2) Make summary file of counts for thresholds 0.1 to 1 for all classes
 load([summarydir 'class\performance_classifier_' classifiername],'all'); %get classlist from classifier
 classlist = all.class;
 for i=1:length(classlist)
