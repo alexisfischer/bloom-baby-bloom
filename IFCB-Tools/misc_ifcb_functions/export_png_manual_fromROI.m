c1c829ba395fd9933ad1cee611b2bea63933f9a5
Alexis Fischer <afischer9@gmail.com>
2023-03-08 11:43:04 -0800

    CCSv11 summary files

---
+++ IFCB-Tools/misc_ifcb_functions/export_png_manual_fromROI.m
@@ -0,0 +1,54 @@
+%USER SET PATHS
+%where are your manual classification results? same resultpath as for manual_classify
+resultpath = 'D:\Shimada\manual\'; % manual annotation file location
+outputpath = 'D:\general\ROI_images\'; %USER where to write out pngs
+roibasepath = 'D:\Shimada\data\xxxx\'; %USER where are your ROIs, put xxxx to mark loaction so of year digits
+%urlbase = 'http://ifcb-data.whoi.edu/mvco/'; %USER where is your dashboard\web server
+
+resultfilelist = dir([resultpath 'D*.mat']);
+resultfilelist = char(resultfilelist.name);
+resultfilelist = cellstr(resultfilelist(:,1:end-4));
+
+for filecount = 1:length(resultfilelist),
+    resultfile = char(resultfilelist(filecount));    
+    load([resultpath resultfile])
+
+    %USER CHOOSE A LINE AND EDIT FOR YOUR CASE
+    %category = class2use_manual; %use this syntax to export ALL categories
+    category = {'Pseudo-nitzschia_small_1cell', 'Pseudo-nitzschia_large_1cell',...
+        'Pseudo-nitzschia_small_2cell', 'Pseudo-nitzschia_large_2cell',...
+        'Pseudo-nitzschia_small_3cell', 'Pseudo-nitzschia_large_3cell',...
+        'Pseudo-nitzschia_small_4cell', 'Pseudo-nitzschia_large_4cell',...
+        'Pseudo-nitzschia_small_5cell', 'Pseudo-nitzschia_large_5cell',...
+        'Pseudo-nitzschia_small_6cell', 'Pseudo-nitzschia_large_6cell'}; %use this syntax to export ONLY the listed categories
+    %category = setdiff(class2use_manual, {'bad' 'ciliate' 'detritus'});  %use this syntax to export all EXCEPT the listed categories
+    %category = setdiff(class2use_manual, {'other' 'misc_nano'});  %use this syntax to export all EXCEPT the listed categories
+
+    disp(resultfile)
+    %make subdirs for tiffs
+    for count = 1:length(category),
+        if ~exist([outputpath char(category(count))], 'dir'),
+            mkdir([outputpath char(category(count))]);
+        end;
+    end;
+    if resultfile(1) == 'D'
+        roipath = [roibasepath resultfile(1:9) filesep];
+        roipath = regexprep(roipath, 'xxxx', resultfile(2:5));
+    else
+        roipath = [roibasepath resultfile(1:14) filesep];
+        roipath = regexprep(roipath, 'xxxx', resultfile(7:10));
+    end;
+    
+    %loop over classes and save pngs to subdirs
+    for count2 = 1:length(category);
+        classnum = strmatch(category(count2), class2use_manual, 'exact');
+        if isempty(classnum),
+            disp(['Category is missing from class2use: ' category{count2}])
+        else
+            ind = find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum));
+            %   ind = find(classlist(:,2) == classnum);  %MANUAL ONLY
+            roinum = classlist(ind,1);
+            export_png_from_ROIlist([roipath resultfile], [outputpath filesep category{count2}], roinum);
+        end;
+    end;
+end
