798d2ddd10c8c93cab5e49dd20346a33e73f52f9
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2023-03-08 18:35:08 -0800

    summary files

--- IFCB-Tools/TB_plots_versus_Manual_CCS.m
+++ IFCB-Tools/TB_plots_versus_Manual_CCS.m
@@ -1,10 +1,10 @@
 %% plot manual vs classifier results for Shimada
 clear;
 classifiername='CCS_v11';
-class2do_full='Pseudo-nitzschia_large_3cell,Pseudo-nitzschia_small_3cell';
+class2do_full='Pseudo-nitzschia_large_1cell,Pseudo-nitzschia_small_1cell';
 
 filepath = '~/Documents/MATLAB/bloom-baby-bloom/';
-outpath = [filepath 'IFCB-Data/Shimada/class/' classifiername '/Figs/'];
+outpath = [filepath 'IFCB-Data/Shimada/threshold/' classifiername '/Figs/'];
 class_indices_path=[filepath 'IFCB-Tools/convert_index_class/class_indices.mat'];   
 addpath(genpath('~/Documents/MATLAB/ifcb-analysis/'));
 addpath(genpath(filepath));
@@ -50,12 +50,14 @@ subplot = @(m,n,p) subtightplot (m, n, p, [0.05 0.05], [0.1 0.1], [0.14 0.04]);
 
 [~,label]=get_class_ind(class2do_full, 'all',class_indices_path);
 
+ymax=ceil(max(max([auto./ml_analyzedTB,man./ml_analyzed])));
+
 subplot(2,1,1);
 stem(mdateTB,auto./ml_analyzedTB,'k-','Linewidth',.5,'Marker','none'); hold on; %This adjusts the automated counts by the chosen slope. 
     plot(matdate,man./ml_analyzed,'r*','Markersize',6,'linewidth',.8);
     datetick('x', 'mmm', 'keeplimits');        
     set(gca,'xgrid','on','tickdir','out','xlim',[datetime('2019-06-01') datetime('2019-09-15')],...
-        'xticklabel',{},'fontsize',10); 
+        'xticklabel',{},'ylim',[0 ymax],'fontsize',10); 
     ylabel('2019','fontsize',12);    
     title([char(label) ' (cells mL^{-1})'],'fontsize',12); 
  legend('classifier','manual','location','nw');legend boxoff;
@@ -63,7 +65,7 @@ stem(mdateTB,auto./ml_analyzedTB,'k-','Linewidth',.5,'Marker','none'); hold on; 
 subplot(2,1,2);
 stem(mdateTB,auto./ml_analyzedTB,'k-','Linewidth',.5,'Marker','none'); hold on; %This adjusts the automated counts by the chosen slope. 
     plot(matdate,man./ml_analyzed,'r*','Markersize',6,'linewidth',.8);
-    set(gca,'xgrid','on','tickdir','out',...
+    set(gca,'xgrid','on','tickdir','out','ylim',[0 ymax],...
         'xlim',[datetime('2021-06-01') datetime('2021-09-15')],'fontsize',10); 
     datetick('x', 'mmm', 'keeplimits');    
     ylabel('2021','fontsize',12);    
