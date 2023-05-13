7bdb7c06ba3e165d5df46dd221dc741ab48fc280
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2023-05-13 13:08:05 -0700

    width 3.8

--- NOAA/Shimada/plot_PN_width_histogram_manual.m
+++ NOAA/Shimada/plot_PN_width_histogram_manual.m
@@ -2,16 +2,16 @@
 % complete dataset
 clear;
 
+micron_factor=3.8;
 filepath = '~/Documents/MATLAB/bloom-baby-bloom/';
 outpath = [filepath 'NOAA/Shimada/Figs/'];
 class_indices_path=[filepath 'IFCB-Tools/convert_index_class/class_indices.mat'];   
 addpath(genpath('~/Documents/MATLAB/ifcb-analysis/'));
 addpath(genpath(filepath));
 
-load([filepath 'IFCB-Data/Shimada/manual/summary_PN_width_manual'],...
+load([filepath 'IFCB-Data/Shimada/manual/summary_PN_width_manual_micron-factor' num2str(micron_factor) ''],...
     'ml_analyzed','mdate','filelist','PN*','micron_factor');
 
-micron_factor=3.8;
 dt=datetime(mdate,'ConvertFrom','datenum');
 
 [lat,lon,ia,filelist] = match_IFCBdata_w_Shimada_lat_lon(filepath,filelist);
@@ -36,24 +36,23 @@ subplot = @(m,n,p) subtightplot (m, n, p, [0.07 0.07], [0.12 0.07], [0.14 0.04])
 %where opt = {gap, width_h, width_w} describes the inner and outer spacings.  
 
 subplot(2,1,1);
-total=sum([length(PNwidth_small(i19).total)',length(PNwidth_large(i19).total)'])
-histogram([PNwidth_small(i19).total],1:.5:9); hold on
-histogram([PNwidth_large(i19).total],1:.5:9); hold on
-xline(3.5,'-r','linewidth',1); hold on
+histogram([PNwidth_small(i19).total],1:.25:9); hold on
+histogram([PNwidth_large(i19).total],1:.25:9); hold on
+xline(3.75,'-r','linewidth',1); hold on
 set(gca,'xlim',[1 9],'xtick',1:2:9,'xticklabel',{},'fontsize',10,'tickdir','out');
 ylabel('Count','fontsize',11)
 legend('"small"','"large"')
 title(['' num2str(micron_factor) ' pixels/\mum'],'fontsize',11)
-text(1.2,750,{'2019';'n=' num2str(length(,'fontsize',11,'color','k'); hold on
+text(1.2,350,'2019','fontsize',11,'color','k'); hold on
 
 subplot(2,1,2);
-histogram([PNwidth_small(i21).total],1:.5:9); hold on
-histogram([PNwidth_large(i21).total],1:.5:9); hold on
-xline(3.5,'-r','linewidth',1); hold on
+histogram([PNwidth_small(i21).total],1:.25:9); hold on
+histogram([PNwidth_large(i21).total],1:.25:9); hold on
+xline(3.75,'-r','3.75 \mum','linewidth',1); hold on
 set(gca,'xlim',[1 9],'xtick',1:2:9,'fontsize',10,'tickdir','out');
 ylabel('Count','fontsize',11)
 xlabel('Width (\mum)','fontsize',11)
-text(1.2,750,'2021','fontsize',11,'color','k'); hold on
+text(1.2,350,'2021','fontsize',11,'color','k'); hold on
   
 % set figure parameters
 exportgraphics(gcf,[outpath 'PN_width_histogram_manual_CCS_micron-factor' num2str(micron_factor) '.png'],'Resolution',100)    
