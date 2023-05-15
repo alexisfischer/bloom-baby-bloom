d40129b3500c12712d05ed4343e171b7328db33f
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2023-05-15 12:17:12 -0700

    new PN width plots

--- NOAA/Shimada/plot_stations_Hake.m
+++ NOAA/Shimada/plot_stations_Hake.m
@@ -1,26 +1,35 @@
 %% plot station map of hake, NCC, and Newport lines
 clear;
-filepath='~/Documents/MATLAB/bloom-baby-bloom/NOAA/';
-addpath(genpath('~/Documents/MATLAB/bloom-baby-bloom/')); % add new data to search path
-addpath(genpath('~/Documents/MATLAB/ifcb-analysis/')); % add new data to search path
-addpath(genpath(filepath)); % add new data to search path
 
-load([filepath 'Shimada/Data/IFCB_NOAA_UCSC_lat_lon_dt'],'NOAA');
-d=length(NOAA.dt)-length(NOAA.filelist); NOAA.dt(end-d+1:end)=[];
-iN=find(NOAA.dt>=datetime('01-Jan-2021'));
+filepath = '~/Documents/MATLAB/bloom-baby-bloom/';
+addpath(genpath('~/Documents/MATLAB/ifcb-analysis/'));
+addpath(genpath(filepath));
+
+load([filepath 'IFCB-Data/Shimada/class/summary_PN_allTB_CCS_NOAA-OSU_v7.mat'],'mdateTB','filelistTB');
+dt=datetime(mdateTB,'ConvertFrom','datenum');
+
+%%%% match data with lat lon
+[lat,lon,ia,filelistTB] = match_IFCBdata_w_Shimada_lat_lon(filepath,filelistTB); dt=dt(ia); 
+
+%%% use 2021 data
+idx=dt>datetime('01-Jan-2020'); dt=dt(idx); lat=lat(idx); lon=lon(idx); 
+
+%%%% remove data south of 40 N and the Strait so ~equivalent between 2019 and 2021
+idx=find(lat<40); dt(idx)=[]; lat(idx)=[]; lon(idx)=[]; 
+idx=find(lat>47.5 & lon>-124.7); dt(idx)=[]; lat(idx)=[]; lon(idx)=[]; 
+
+clearvars idx ia mdateTB filelistTB dt
 
 %%%% plot
-figure; set(gcf,'color','w','Units','inches','Position',[1 1 3 5]); 
+figure; set(gcf,'color','w','Units','inches','Position',[1 1 3 4.5]); 
 c=brewermap(3,'PuBuGn');
-
-states=load([filepath 'Shimada/Data/USwestcoast_pol']);
-load([filepath 'Shimada/Data/coast_CCS'],'coast');
+states=load([filepath 'NOAA/Shimada/Data/USwestcoast_pol']);
+load([filepath 'NOAA/Shimada/Data/coast_CCS'],'coast');
 fillseg(coast); dasp(42); hold on;
-plot(states(:,1),states(:,2),'k'); hold on;
-set(gca,'ylim',[34 49],'xlim',[-127 -120],'fontsize',11,'tickdir','out','box','on','xaxisloc','bottom');
-
-hake=scatter(NOAA.lon(iN),NOAA.lat(iN),10,c(2,:),'filled'); hold on
+plot(states.lon,states.lat,'k'); hold on;
+set(gca,'ylim',[39.9 49],'xlim',[-128.5 -122.3],'xtick',-129:2:-121,'fontsize',10,'tickdir','out','box','on','xaxisloc','bottom');
+hake=scatter(lon,lat,10,c(2,:),'filled'); hold on
 
 % set figure parameters%
-    exportgraphics(gca,[filepath 'Shimada/Figs/stationmap_Hake.png'],'Resolution',100)    
+    exportgraphics(gca,[filepath 'NOAA/Shimada/Figs/stationmap_Hake.png'],'Resolution',300)    
 
--- NOAA/Shimada/plot_stations_Hake_NCC_Newport.m
+++ NOAA/Shimada/plot_stations_Hake_NCC_Newport.m
@@ -5,12 +5,12 @@ addpath(genpath('~/Documents/MATLAB/bloom-baby-bloom/')); % add new data to sear
 addpath(genpath('~/Documents/MATLAB/ifcb-analysis/')); % add new data to search path
 addpath(genpath(filepath)); % add new data to search path
 
-load([filepath 'SeascapesProject/Data/NCC_stationlist'],'N');
+load([filepath 'Shimada/Data/NCC_stationlist'],'N');
 
-load([filepath 'SeascapesProject/Data/SeascapeSummary_NOAA-OSU-UCSC'],'S');
+load([filepath 'Shimada/Data/SeascapeSummary_NOAA-OSU-UCSC'],'S');
 iO=find((S.dt>datetime('01-March-2019')) & strcmp(S.group,{'OSU'})); %find data from one osu cruise
 
-load([filepath 'SeascapesProject/Data/IFCB_NOAA_UCSC_lat_lon_dt'],'NOAA');
+load([filepath 'Shimada/Data/IFCB_NOAA_UCSC_lat_lon_dt'],'NOAA');
 d=length(NOAA.dt)-length(NOAA.filelist); NOAA.dt(end-d+1:end)=[];
 iN=find(NOAA.dt>=datetime('01-Jan-2021'));
 
@@ -21,7 +21,7 @@ c=brewermap(3,'PuBuGn');
 states=load([filepath 'Shimada/Data/USwestcoast_pol']);
 load([filepath 'Shimada/Data/coast_CCS'],'coast');
 fillseg(coast); dasp(42); hold on;
-plot(states(:,1),states(:,2),'k'); hold on;
+plot(states.lon,states.lat,'k'); hold on;
 set(gca,'ylim',[34 49],'xlim',[-127 -120],'fontsize',11,'tickdir','out','box','on','xaxisloc','bottom');
 
 hake=scatter(NOAA.lon(iN),NOAA.lat(iN),10,c(2,:),'filled'); hold on
@@ -32,6 +32,6 @@ legend boxoff;
     legend boxoff; lh.FontSize = 11; hp=get(lh,'pos');
     lh.Position=[hp(1)*.75 hp(2)*.9 hp(3) hp(4)]; hold on    
 
-% set figure parameters%
+%% set figure parameters%
 print(gcf,'-dpng','-r100',[filepath 'SeascapesProject/Figs/stationmap_Hake-NCCE-Newport.png']);
 hold off 
