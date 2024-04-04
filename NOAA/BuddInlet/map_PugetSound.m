c66f1a66bd072ed1c094b3a99f37867a4390a255
afisch <afischer9@gmail.com>
2024-04-04 12:10:34 -0700

    attempted map

--- NOAA/BuddInlet/map_PugetSound.m
+++ NOAA/BuddInlet/map_PugetSound.m
@@ -7,84 +7,88 @@ addpath(genpath('~/Documents/MATLAB/ifcb-analysis/'));
 addpath(genpath(filepath));
 addpath(genpath('~/Documents/MATLAB/bloom-baby-bloom'));
 
-workingFolder = tempdir;
-files = gunzip('~/Downloads/gshhg-gmt-2.3.7.tar.gz', workingFolder);
-filename = files{1};
 
-indexfile = gshhs(filename, 'createindex');
-latlim = [46.5  49];
-lonlim = [-125 -122];
-S = gshhs(filename, latlim, lonlim);
-delete(filename)
-delete(indexfile)
-
-S(1)
-levels = [S.Level];
-unique(levels)
-S(104).LevelString
-
-L1 = S(levels == 1);
-L2 = S(levels == 2);
-
-figure
-axesm('mercator', 'MapLatLimit', latlim, 'MapLonLimit', lonlim)
-gridm; mlabel; plabel
-geoshow([L1.Lat], [L1.Lon], 'Color', 'blue')
-geoshow([L2.Lat], [L2.Lon], 'Color', 'red')
-tightmap
+%% can't figure out how to get waterbody
+mappath='~/Downloads/NHD_H_17110016_HU8_Shape/Shape/';
+%Sw = shaperead([mappath 'NHDWaterbody.shp']);
+Sw = readgeotable([mappath 'NHDWaterbody.shp']);
 
 %%
-m_proj('albers equal-area','lat',[48.5 46.8],'long',[-125 -122],'rect','on');
-m_gshhs_f('save',[filepath 'Figs/PugetSound_map']);
+% %%
+% mappath='~/Downloads/WBD_17_HU2_Shape/Shape/';
+% 
+% figure('Units','inches','Position',[1 1 3.5 4],'PaperPositionMode','auto');        
+% 
+% Sw = shaperead([mappath 'WBDHU12.shp'],'UseGeoCoords',true);
+% 
+% gx=geoaxes('Basemap','grayland');
+% geoplot(gx,[Sw.Lat],[Sw.Lon],'color','w','linewidth',1.5); hold on
+% geolimits(gx,[46.8 48.2],[-123.2 -122.2])
+% 
+% lat=[47.04571 48.08195 47.70521 47.39728 48.03791 48.0569];
+% lon=[-122.90702 -123.04609 -122.62655 -122.43893 -122.86722 -122.69488];
+% geoscatter(gx,lat,lon,35,'filled','k'); hold on
+% 
+% gx = gca; gx.Scalebar.Visible;
+% gx.Scalebar.BackgroundAlpha = 1;
 
-load([filepath 'Figs/PugetSound_map'],'ncst');
+% %%
+% m_proj('albers equal-area','lat',[48.5 46.8],'long',[-125 -122],'rect','on');
+% m_gshhs_f('save',[filepath 'Figs/PugetSound_map']);
+% load([filepath 'Figs/PugetSound_map'],'ncst');
+
+%%
+%%%% Puget Sound Map
+figure('Units','inches','Position',[1 1 3.5 4],'PaperPositionMode','auto');        
+m_proj('albers equal-area','lat',[48.2 46.9],'long',[-123.3 -122.2],'rect','on'); hold on;
+
+m_gshhs_f('patch',[.8 .8 .8],'edgecolor','none');  hold on;
+m_grid('linestyle','none','linewidth',1,'tickdir','out',...
+     'xaxisloc','top','yaxisloc','left','fontsize',12); hold on;  
+
+m_usercoast([filepath 'Data/PNW_watershed'],'color','w','linewidth',1.5)
+
+m_ruler(.05,[.02 .3],3,'ticklen',.02,'fontsize',10);
+
+m_line(-122.90702,47.04571,'marker','.','markersize',12,'color','k');
+m_text(-122.90702-.11,47.04571-.03,{'BI'},'fontsize',10);
+
+m_line(-123.04609,48.08195,'marker','.','markersize',12,'color','k');
+m_text(-123.04609-.13,48.08195-.03,{'SB'},'fontsize',10);
+
+m_line(-122.62655,47.70521,'marker','.','markersize',12,'color','k');
+m_text(-122.62655-.13,47.70521-.03,{'LB'},'fontsize',10);
+
+m_line(-122.43893,47.39728,'marker','.','markersize',12,'color','k');
+m_text(-122.43893-.13,47.39728-.03,{'QH'},'fontsize',10);
+
+m_line(-122.86722,48.03791,'marker','.','markersize',12,'color','k');
+m_text(-122.86722-.13,48.03791-.03,{'DB'},'fontsize',10);
+
+m_line(-122.69488,48.0569,'marker','.','markersize',12,'color','k');
+m_text(-122.69488-.13,48.0569-.03,{'MB'},'fontsize',10);
+
+hold on;
+
+% set figure parameters
+exportgraphics(gcf,[filepath 'Figs/Map_PugetSound.png'],'Resolution',300)    
+hold off
 
 %% BuddInlet Map
 figure('Units','inches','Position',[1 1 3.5 4],'PaperPositionMode','auto');        
 m_proj('albers equal-area','lat',[47.13 47.04],'long',[-122.94 -122.88],'rect','on');
 
-m_gshhs_f('patch',[.8 .8 .8],'edgecolor','none');  
+m_gshhs_f('patch',[.8 .8 .8],'edgecolor','none'); hold on
 m_grid('linestyle','none','linewidth',1,'tickdir','out',...
      'xaxisloc','top','yaxisloc','left','fontsize',12);  
 
 m_ruler(.05,[.02 .35],'ticklen',.01);
 
-%m_line(-122.905,47.0465,'marker','.','markersize',12,'color','k');
+m_usercoast([filepath 'Data/PNW_watershed'],'color','w','linewidth',1.5)
+
 m_line(-122.906843,47.047,'marker','.','markersize',12,'color','k');
 
 
 %% set figure parameters
 exportgraphics(gcf,[filepath 'Figs/Map_BuddInlet.png'],'Resolution',300)    
-hold off
-
-%% Puget Sound Map
-figure('Units','inches','Position',[1 1 3.5 4],'PaperPositionMode','auto');        
-m_proj('albers equal-area','lat',[48.2 47],'long',[-123.3 -122.2],'rect','on');
-
-m_gshhs_f('patch',[.8 .8 .8],'edgecolor','none');  
-m_grid('linestyle','none','linewidth',1,'tickdir','out',...
-     'xaxisloc','top','yaxisloc','left','fontsize',12);  
-
-m_ruler(.05,[.02 .35],3,'ticklen',.02,'fontsize',10);
-
-m_line(-122.90702,47.04571,'marker','.','markersize',12,'color','k');
-m_text(-122.86,47.04571,{'Budd Inlet'},'fontsize',10,'fontweight','bold');
-
-m_line(-123.04609,48.08195,'marker','.','markersize',12,'color','k');
-m_text(-123.04609-.05,48.08195-.05,{'SB'},'fontsize',10);
-
-m_line(-122.62655,47.70521,'marker','.','markersize',12,'color','k');
-m_text(-122.62655-.05,47.70521-.05,{'LB'},'fontsize',10);
-
-m_line(-122.43893,47.39728,'marker','.','markersize',12,'color','k');
-m_text(-122.43893-.05,47.39728-.05,{'QH'},'fontsize',10);
-
-m_line(-122.86722,48.03791,'marker','.','markersize',12,'color','k');
-m_text(-122.86722-.05,48.03791-.05,{'DB'},'fontsize',10);
-
-m_line(-122.69488,48.0569,'marker','.','markersize',12,'color','k');
-m_text(-122.69488-.05,48.0569-.05,{'MB'},'fontsize',10);
-
-% set figure parameters
-exportgraphics(gcf,[filepath 'Figs/Map_PugetSound.png'],'Resolution',300)    
 hold off
